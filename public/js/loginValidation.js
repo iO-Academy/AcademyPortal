206cee0fc2d69603ce9a77196600f45d1d426acf
C0deJack <jackwoodpearce@gmail.com>
2018-05-15 15:29:24 +0100

    We have created fat arrow fucntions for ES6 and changed a function to change the class rather than the styling directly

--- public/js/loginValidation.js
+++ public/js/loginValidation.js
@@ -1,47 +1,47 @@
-function isValidEmail(elementID) {
-    let inputVal = document.getElementById(elementID);
+let isValidEmail = (elementID) => {
+    let inputVal = document.getElementById(elementID)
+    let userEmailWarning = document.getElementById('userEmailWarning')
     let email = inputVal.value.trim();
-    let re = new RegExp("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$");
-
+    let re = new RegExp("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$")
+console.log(re.test(email))
     if (re.test(email)) {
-        inputVal.style.backgroundColor = "";
+        userEmailWarning.classList.remove('error')
         return true
     } else {
-        inputVal.style.backgroundColor = "pink";
+        userEmailWarning.classList.add('error')
         return false
     }
 }
 
-function postLoginDetails(inputs){
-    fetch("/api/login",
+let validateToPostInputs = () => {
+    let cleanedEmailInput = encodeURI(document.getElementById('userEmail').value)
+    let cleanedPasswordInput = encodeURI(document.getElementById('password').value)
+
+    if (isValidEmail('userEmail')) {
+        let emailPasswordValues = {'email' : cleanedEmailInput, 'password': cleanedPasswordInput}
+        return emailPasswordValues
+    } else {
+        document.getElementById('submitWarning').textContent = 'Field input error'
+    }
+}
+
+let loginDetails = (path, data) => {
+    fetch(`/api/${path}`,
         {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             method: "POST",
-            body: JSON.stringify({
-                "userEmail": inputs['email'],
-                "password": inputs['password']
-            })
+            body: JSON.stringify(data)
         })
         .then(function(data){return data.json()})
 }
 
-function validateToPostInputs() {
-    let cleanedEmailInput = encodeURI(document.getElementById('userEmail').value);
-    let cleanedPasswordInput = encodeURI(document.getElementById('password').value);
-
-    if (isValidEmail('userEmail')) {
-        let emailPasswordValues = {'email' : cleanedEmailInput, 'password': cleanedPasswordInput};
-        return emailPasswordValues;
-    } else {
-        document.getElementById('submitWarning').textContent = 'Field input error'
-    }
-}
-
-document.getElementById('formID').addEventListener('submit', function(e){
-    e.preventDefault();
-    let validInputs = validateToPostInputs();
-    postLoginDetails(validInputs);
-});
+let loginForm = document.getElementById('formID')
+loginForm.addEventListener('submit', (e) => {
+    let validInputs
+    e.preventDefault()
+    validInputs = validateToPostInputs()
+    loginDetails(validInputs)
+})
