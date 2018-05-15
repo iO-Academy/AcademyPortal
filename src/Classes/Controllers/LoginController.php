b2a72856136235e06791710977fd5a596855898d
obwansan <kob123@hotmail.co.uk>
2018-05-15 14:45:15 +0100

    added controller class that uses user ModelObject for verification and returns json

--- src/Classes/Controllers/LoginController.php
+++ src/Classes/Controllers/LoginController.php
@@ -1,15 +1,49 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: academy
- * Date: 14/05/2018
- * Time: 15:53
- */
 
 namespace Portal\Controllers;
 
+use \Slim\Http\Request as Request;
+use \Slim\Http\Response as Response;
+use Portal\Models\UserModel;
 
 class LoginController
 {
+    private $userModel;
 
+    /**
+     * LoginController constructor.
+     * @param UserModel $userModel userModel object dependency
+     */
+    public function __construct(UserModel $userModel)
+    {
+        $this->userModel = $userModel;
+    }
+
+    /**
+     * @param Request $request HTTP request
+     * @param Response $response HTTP response
+     *
+     * @return string JSON message and status code
+     */
+    public function __invoke(Request $request, Response $response)
+    {
+        $data = ['success' => false, 'msg' => 'Incorrect email or password.', 'data' => []];
+        $statusCode = 401;
+        $_SESSION['loggedIn'] = false;
+
+        $parsedBody = $request->getParsedBody();
+        $user = $this->userModel->getUserByEmail($parsedBody['userEmail']);
+
+        $result = $this->userModel->userLoginVerify($parsedBody['userEmail'], $parsedBody['password'], $user);
+
+        if($result) {
+            $data['success'] = $result;
+            $data['msg'] = 'Valid user';
+            $_SESSION['loggedIn'] = true;
+            $statusCode = 200;
+        }
+
+        return $response->withJson($data, $statusCode);
+
+    }
 }
