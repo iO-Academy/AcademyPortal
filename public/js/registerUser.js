3996dd5227f3bb2978fb352b535a7e0b8e429d29
marty-crane <kyam.lh@googlemail.com>
2018-05-17 11:42:32 +0100

    editing of promise to send new user details to db

--- public/js/registerUser.js
+++ public/js/registerUser.js
@@ -1,6 +1,4 @@
 
-document.getElementById("randomPassword").readOnly = true;
-
 let sendNewUserDetails = async (data) => {
 
     let response =  await fetch(`/api/registerUser`,
@@ -14,29 +12,27 @@ let sendNewUserDetails = async (data) => {
             body: JSON.stringify(data)
         })
         .then(data => data.json())
-        .then(function(data){return data})
-    console.log(response)
     return response
 }
 
 let getInputs = () =>{
     let cleanedEmailInput = encodeURI(document.getElementById('emailNewUser').value)
     let cleanedPasswordInput = encodeURI(document.getElementById('randomPassword').value)
-    return {'userEmail' : cleanedEmailInput, 'password' : cleanedPasswordInput}
+    return {'email' : cleanedEmailInput, 'password' : cleanedPasswordInput}
 }
 
 let newUserForm = document.getElementById('registerUserForm')
 newUserForm.addEventListener('submit', async (e) => {
     e.preventDefault()
-    let inputs = getInputs()
-    let response = await sendNewUserDetails(inputs)
-    let messageBox = document.getElementById("message")
+    let data = getInputs(),
+        messageBox = document.getElementById("message"),
+        response = await sendNewUserDetails(data)
 
-    if(!response['success']) {
-        messageBox.innerText = 'Error adding user to database'
-        messageBox.classList.add('error')
-    } else if (response['success'] === true) {
-        messageBox.innerText = `New user added email:${inputs['userEmail']} password:${inputs['password']}`
-        messageBox.classList.remove('error')
-    }
+        if(!response['success']) {
+            messageBox.innerText = 'Error adding user to database'
+            messageBox.classList.add('error')
+        } else if (response['success'] === true) {
+            messageBox.innerText = `New user added email:' ${inputs['email']} ' password:' ${inputs['password']}'`
+            messageBox.classList.remove('error')
+        }
 })
