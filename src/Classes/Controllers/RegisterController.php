f125a9d3e362d3c99b93db56b15081071211bbed
marty-crane <kyam.lh@googlemail.com>
2018-05-17 16:06:14 +0100

    Type hinting added to controller, styling issues resolved

--- src/Classes/Controllers/RegisterController.php
+++ src/Classes/Controllers/RegisterController.php
@@ -3,19 +3,21 @@
 namespace Portal\Controllers;
 
 use Slim\Views\PhpRenderer;
+use \Slim\Http\Request as Request;
+use \Slim\Http\Response as Response;
 
 class RegisterController
 {
     private $renderer;
     private $password;
 
-    function __construct(PhpRenderer $renderer, $password)
+    function __construct(PhpRenderer $renderer,string $password)
     {
         $this->renderer = $renderer;
         $this->password = $password;
     }
 
-    function __invoke($request, $response, $args)
+    function __invoke(Request $request,Response $response,array $args)
     {
         $args['password'] = $this->password;
         if ($_SESSION['loggedIn'] === true) {
