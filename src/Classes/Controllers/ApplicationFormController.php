85a67dc6986b3e25e3399122a6e42ba4fe79d618
Nick Santiago Blair <nsantiagoblair@gmail.com>
2018-05-17 13:04:13 +0100

    fetch assoc only if logged in

--- src/Classes/Controllers/ApplicationFormController.php
+++ src/Classes/Controllers/ApplicationFormController.php
@@ -16,43 +16,21 @@ class ApplicationFormController
     }
 
     function __invoke(Request $request, Response $response, $args)
-    {
-        
-
-        
-
-        // if ($_SESSION['loggedIn'] === true) {
-
-            $cohorts = $this->applicationFormModel->getCohorts();
-            $hearAbout = $this->applicationFormModel->getHearAbout();
-        $statusCode = 200;
-           $data = [
-               'success' => true, 
-               'msg' => 'Retrieved dropdown info.', 
-               'data' => [
-                    'cohorts' => $cohorts,
-                    'hearAbout' => $hearAbout,
-               ]
+    {   
+        //Line below is here for testing. Should be removed after code review!!!
+        // $_SESSION['loggedIn'] = true; 
+        if ($_SESSION['loggedIn'] === true) {
+            $statusCode = 200;
+            $data = [
+                'success' => true, 
+                'msg' => 'Retrieved dropdown info.', 
+                'data' => [
+                        'cohorts' => $this->applicationFormModel->getCohorts(),
+                        'hearAbout' => $this->applicationFormModel->getHearAbout(),
+                ]
             ];
-            echo '<pre>' . var_export($data, true) . '</pre>';
 
             return $response->withJson($data, $statusCode);
-        // } 
-
-
-        
-
-        // $parsedBody = $request->getParsedBody();
-        // $user = $this->userModel->getUserByEmail($parsedBody['userEmail']);
-        // $result = $this->userModel->userLoginVerify($parsedBody['userEmail'], $parsedBody['password'], $user);
-
-        // if($result) {
-        //     $data['success'] = $result;
-        //     $data['msg'] = 'Valid user';
-        //     $_SESSION['loggedIn'] = true;
-        //     $statusCode = 200;
-        // }
-
-        // return $response->withJson($data, $statusCode);
+        } 
     }
 }
