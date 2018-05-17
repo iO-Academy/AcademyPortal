b6a836966d9ad0953ed76969b3158754441d87e7
“Osama-Tahboub1” <o.tahboob2000@gmail.com>
2018-05-17 10:26:46 +0100

    replaced route to home page with new route to dependency

--- src/routes.php
+++ src/routes.php
@@ -3,11 +3,15 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-$app->get('/', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-    return $this->renderer->render($response, 'index.phtml', $args);
-});
+//$app->get('/', function (Request $request, Response $response, array $args) {
+//    $this->logger->info("Slim-Skeleton '/' route");
+//    return $this->renderer->render($response, 'index.phtml', $args);
+//});
 
+//FrontEnd
+$app->get('/','HomePageController');
+$app->get('/admin', 'AdminController');
+
+//Backend
 $app->post('/api/login', 'LoginController');
-$app->get('/admin', 'adminController');
+
