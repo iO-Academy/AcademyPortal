b1dafe9610c6dca2c8489ef2297c3651296f34ba
“Osama-Tahboub1” <o.tahboob2000@gmail.com>
2018-05-17 11:18:15 +0100

    added return typehinting to invoke function

--- src/Classes/Controllers/LoginController.php
+++ src/Classes/Controllers/LoginController.php
@@ -29,7 +29,7 @@ class LoginController
      *
      * @return string JSON message and status code
      */
-    public function __invoke(Request $request, Response $response)
+    public function __invoke(Request $request, Response $response):Response
     {
         $data = ['success' => false, 'msg' => 'Incorrect email or password.', 'data' => []];
         $statusCode = 401;
