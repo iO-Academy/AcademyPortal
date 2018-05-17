8045648cd182dbeca8aea1ee505dda5ebe2e48af
“Osama-Tahboub1” <o.tahboob2000@gmail.com>
2018-05-17 12:15:23 +0100

    made function is validEmail more generic and moved somefunctionality to validate inputs function

--- public/js/loginValidation.js
+++ public/js/loginValidation.js
@@ -1,21 +1,16 @@
-let isValidEmail = (elementID) => {
-    let inputVal = document.getElementById(elementID)
-    let userEmailWarning = document.getElementById('userEmailWarning')
-    let email = inputVal.value.trim();
+let isValidEmail = (inputEmail) => {
+    let email = inputEmail.trim()
     let regEx = new RegExp("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$")
-    if (regEx.test(email)) {
-        userEmailWarning.classList.remove('error')
-        return true
-    } else {
-        userEmailWarning.classList.add('error')
-        return false
-    }
+    return regEx.test(email)
 }
 
 let validateInputs = (cleanedEmailInput, cleanedPasswordInput) => {
-    if (isValidEmail('userEmail')) {
+    let userEmailWarning = document.getElementById('userEmailWarning')
+    if (isValidEmail(cleanedEmailInput)) {
+        userEmailWarning.classList.remove('error')
         return {'userEmail' : cleanedEmailInput, 'password': cleanedPasswordInput}
     } else {
+        userEmailWarning.classList.add('error')
         document.getElementById('submitWarning').textContent = 'Field input error'
     }
 }
