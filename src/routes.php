bf897e3cf3de00bc7262220e9a8da1264d05efe5
“Osama-Tahboub1” <o.tahboob2000@gmail.com>
2018-05-17 10:40:57 +0100

    removed comments from routes

--- src/routes.php
+++ src/routes.php
@@ -3,11 +3,6 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-//$app->get('/', function (Request $request, Response $response, array $args) {
-//    $this->logger->info("Slim-Skeleton '/' route");
-//    return $this->renderer->render($response, 'index.phtml', $args);
-//});
-
 //FrontEnd
 $app->get('/','HomePageController');
 $app->get('/admin', 'AdminController');
