d6504dd19c5878fc04dee1cd633944192951dae3
marty-crane <kyam.lh@googlemail.com>
2018-05-18 09:20:35 +0100

    commented out unusable test

--- tests/Models/RandomPasswordModelTest.php
+++ tests/Models/RandomPasswordModelTest.php
@@ -2,10 +2,7 @@
 
 namespace Tests\Models;
 
-use PHPUnit\Framework\TestCase;
-use Portal\Models\RandomPasswordModel;
-
-class RandomPasswordModelTest extends TestCase
-{
+//class RandomPasswordModelTest extends TestCase
+//{
     //this is untestable as a unit test as it calls from the API
-}
+//}
