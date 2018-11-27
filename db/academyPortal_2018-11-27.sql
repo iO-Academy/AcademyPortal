e7eea2a10ad9d680737098b33facca4f1c45fcc1
jaholt <holt.ja.halfways@gmail.com>
2018-11-27 13:21:34 +0000

    Updated database to actually work (1,0 not yes no)

--- db/academyPortal_2018-11-27.sql
+++ db/academyPortal_2018-11-27.sql
@@ -5,9 +5,9 @@
 # http://www.sequelpro.com/
 # https://github.com/sequelpro/sequelpro
 #
-# Host: 127.0.0.1 (MySQL 5.6.39)
+# Host: 127.0.0.1 (MySQL 5.6.41)
 # Database: academyPortal
-# Generation Time: 2018-05-17 13:13:11 +0000
+# Generation Time: 2018-11-27 13:20:13 +0000
 # ************************************************************
 
 
@@ -18,7 +18,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 
 
 # Dump of table applicants
@@ -35,9 +34,9 @@ CREATE TABLE `applicants` (
   `whyDev` varchar(2000) DEFAULT NULL,
   `codeExperience` varchar(2000) DEFAULT NULL,
   `hearAboutId` int(11) DEFAULT NULL,
-  `eligible` enum('yes','no') DEFAULT NULL,
-  `eighteenPlus` enum('yes','no') DEFAULT NULL,
-  `finance` enum('yes','no') DEFAULT NULL,
+  `eligible` enum('1','0') DEFAULT NULL,
+  `eighteenPlus` enum('1','0') DEFAULT NULL,
+  `finance` enum('1','0') DEFAULT NULL,
   `notes` varchar(2000) DEFAULT NULL,
   `dateTimeAdded` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted` tinyint(4) DEFAULT '0',
@@ -47,13 +46,12 @@ CREATE TABLE `applicants` (
 LOCK TABLES `applicants` WRITE;
 /*!40000 ALTER TABLE `applicants` DISABLE KEYS */;
 
-INSERT INTO `applicants` (`id`, `name`, `email`, `phoneNumber`, `cohortId`, `whyDev`, `codeExperience`, `hearAboutId`, `eligible`, `eighteenPlus`, `finance`, `deleted`)
+INSERT INTO `applicants` (`id`, `name`, `email`, `phoneNumber`, `cohortId`, `whyDev`, `codeExperience`, `hearAboutId`, `eligible`, `eighteenPlus`, `finance`, `notes`, `dateTimeAdded`, `deleted`)
 VALUES
-	(1,'test','test@test.com','000000',4,'test','test',2,'yes','yes','yes',0);
+	(1,'test','test@test.com','000000',4,'test','test',2,'1','','',NULL,'2018-11-15 04:29:59',0);
 
 /*!40000 ALTER TABLE `applicants` ENABLE KEYS */;
 UNLOCK TABLES;
-
 
 
 # Dump of table cohorts
@@ -64,16 +62,12 @@ DROP TABLE IF EXISTS `cohorts`;
 CREATE TABLE `cohorts` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `date` date DEFAULT NULL,
-<<<<<<< HEAD
   `deleted` tinyint(4) DEFAULT '0',
-=======
->>>>>>> story3-applicantRegister
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 LOCK TABLES `cohorts` WRITE;
 /*!40000 ALTER TABLE `cohorts` DISABLE KEYS */;
-
 
 INSERT INTO `cohorts` (`id`, `date`, `deleted`)
 VALUES
@@ -81,7 +75,6 @@ VALUES
 	(2,'2019-02-01',0),
 	(3,'2019-08-01',0),
 	(4,'2020-02-01',0);
-
 
 /*!40000 ALTER TABLE `cohorts` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -95,16 +88,12 @@ DROP TABLE IF EXISTS `hearAbout`;
 CREATE TABLE `hearAbout` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `hearAbout` varchar(150) DEFAULT NULL,
-<<<<<<< HEAD
   `deleted` tinyint(4) DEFAULT '0',
-=======
->>>>>>> story3-applicantRegister
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 LOCK TABLES `hearAbout` WRITE;
 /*!40000 ALTER TABLE `hearAbout` DISABLE KEYS */;
-
 
 INSERT INTO `hearAbout` (`id`, `hearAbout`, `deleted`)
 VALUES
@@ -114,7 +103,6 @@ VALUES
 	(4,'Telepathy',0),
 	(5,'North Korea',0),
 	(6,'Yoda',0);
-
 
 /*!40000 ALTER TABLE `hearAbout` ENABLE KEYS */;
 UNLOCK TABLES;
