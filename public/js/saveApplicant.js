001a47a63f3aeca239d4d4d04990906f14881c7f
charliecog <charlie.coggans@mayden.co.uk>
2018-11-27 11:32:02 +0000

    grabs data from form on submit and submits to API to add new applicant

--- public/js/saveApplicant.js
+++ public/js/saveApplicant.js
@@ -1,9 +1,35 @@
 document.getElementById('submitApplicant').addEventListener('click', function (e) {
     e.preventDefault()
-    let formData = document.querySelectorAll(".form-control")
+    let formData = document.querySelectorAll(".submitApplicant")
+    let data = getCompletedFormData()
+    makeApiRequest(data)
+})
+
+function getCompletedFormData() {
+    let formData = document.querySelectorAll(".submitApplicant")
     let data = {}
     formData.forEach(function (formItem) {
         data[formItem.name] = formItem.value
+        if (formItem.type == 'checkbox') {
+            data[formItem.name] = formItem.checked
+        }
     })
-    console.log(data)
-})
+    return data
+}
+
+function makeApiRequest(data) {
+    return fetch('/api/saveApplicant', {
+        credentials: "same-origin",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        method: 'post',
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(function () {
+            window.location.href = '/admin';
+        })
+
+}
