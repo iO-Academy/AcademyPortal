b9c7cf8f003f54465ad7bf0081ebb47bb77d48d3
Daniel <hoptond848@protonmail.com>
2018-11-27 11:13:44 +0000

    remove unecessary data

--- tests/Controllers/RegisterUserControllerTest.php
+++ tests/Controllers/RegisterUserControllerTest.php
@@ -1,18 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: academy
- * Date: 26/11/2018
- * Time: 15:40
- */
 
 namespace Tests\Controllers;
 
 use PHPUnit\Framework\TestCase;
 use Portal\Controllers\RegisterUserController;
 use Portal\Models\UserModel;
-use Slim\Http\Request;
-use Slim\Http\Response;
 
 class RegisterUserControllerTest extends TestCase
 {
