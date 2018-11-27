d96633a19b9b8cd2e82e4cded3970b30f44061d6
Daniel Coombes <daniel.coombes224@gmail.com>
2018-11-27 10:16:50 +0000

    Made changes to the get applicants function

--- src/Classes/Models/ApplicantModel.php
+++ src/Classes/Models/ApplicantModel.php
@@ -78,7 +78,7 @@ class ApplicantModel
 }
 
     public function getAllApplicants() {
-    $query = $this->db->prepare('SELECT `name`, `email`, `phoneNumber`, `cohortid`, `whyDev`, `codeExperience`, `hearAboutId`, `eligible`, `eighteenPlus`, `fiance`, `notes`, `deleted` FROM `applicants`;');
+    $query = $this->db->prepare('SELECT `name`, `email`, `cohortid` FROM `applicants`;');
     $query->execute();
     $results = $query->fetchAll(\PDO::FETCH_ASSOC);
     return $results;
