054195cff0c4ccd7f60fd5a547e73cb9f906fb6b
Patrick <patrick.kenyon@gmail.com>
2018-11-27 11:18:58 +0000

    created AddHiringPartnerController.php with constructor and empty invoke function

---
+++ src/Classes/Controllers/AddHiringPartnerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Portal\Controllers;
+
+
+use Portal\Models\HiringPartnerModel;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class AddHiringPartnerController
+{
+    private $hiringPartnerModel;
+
+    /**
+     * AddHiringPartnerController constructor instantiates the AddHiringPartnerController
+     * @param $hiringPartnerModel
+     */
+    public function __construct(HiringPartnerModel $hiringPartnerModel)
+    {
+        $this->hiringPartnerModel = $hiringPartnerModel;
+    }
+
+    public function __invoke(Request $request, Response $response)
+    {
+        // pick up data from form here and validate.    
+
+    }
+
+
+}
