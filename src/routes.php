11678b4ed749778bbbcd32402b060e7d7988c328
Burak Tilek <burak.tilek@gmail.com>
2018-11-27 10:53:02 +0000

    addEvent route added for testing purposes

--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,17 @@
 <?php
 
+use Slim\Http\Request;
+use Slim\Http\Response;
+
 //FrontEnd
 $app->get('/','HomePageController');
 $app->get('/admin', 'AdminController');
 $app->get('/register', 'RegisterController');
+$app->get('/addEvent', function (Request $request, Response $response, array $args) {
+
+    // Render index view
+    return $this->renderer->render($response, 'addEvent.phtml', $args);
+});
 
 //Backend
 $app->post('/api/login', 'LoginController');
