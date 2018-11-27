ad797a4a8dabc65908ec5c6468ad165bc5841608
Patrick <patrick.kenyon@gmail.com>
2018-11-27 12:09:32 +0000

    created HiringPartnerModelFactory.php

---
+++ src/Classes/Factories/HiringPartnerModelFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Portal\Factories;
+
+
+use Portal\Models\HiringPartnerModel;
+use Psr\Container\ContainerInterface;
+
+
+class HiringPartnerModelFactory
+{
+
+
+    /**
+     * Creates new HiringPartnerModel with dependencies
+     *
+     * @param ContainerInterface $container
+     * @return HiringPartnerModel object with db connection injected
+     */
+    public function __invoke(ContainerInterface $container): HiringPartnerModel
+    {
+        $db = $container->get('dbConnection');
+        return new HiringPartnerModel($db);
+    }
+}
