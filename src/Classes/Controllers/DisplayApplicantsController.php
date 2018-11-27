db32bb79077fed2fb7fabdea056c29c3bd5ea56c
jaholt <holt.ja.halfways@gmail.com>
2018-11-27 16:11:21 +0000

    Removed line 40, cohesed the work

--- src/Classes/Controllers/DisplayApplicantsController.php
+++ src/Classes/Controllers/DisplayApplicantsController.php
@@ -36,8 +36,7 @@ class DisplayApplicantsController
      */
     public function __invoke(Request $request, Response $response, array $args)
     {
-        $data = $this->applicantModel->getAllApplicants();
-        $args['data'] = $data;
+        $args['data'] = $this->applicantModel->getAllApplicants();
         return $this->renderer->render($response, 'displayApplicants.phtml', $args);
     }
 }
