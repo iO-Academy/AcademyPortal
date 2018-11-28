77e72e2df44ca1c1a38dcd06e85a9d2c0898942f
Patrick <patrick.kenyon@gmail.com>
2018-11-28 10:50:51 +0000

    added error handling and status response message

--- src/Classes/Controllers/AddHiringPartnerController.php
+++ src/Classes/Controllers/AddHiringPartnerController.php
@@ -3,6 +3,7 @@
 namespace Portal\Controllers;
 
 
+use Portal\Entities\HiringPartnerEntity;
 use Portal\Models\HiringPartnerModel;
 use Slim\Http\Request;
 use Slim\Http\Response;
@@ -22,44 +23,39 @@ class AddHiringPartnerController
 
     public function __invoke(Request $request, Response $response)
     {
+
+        $data = [
+            'success' => false,
+            'msg' => 'Hiring partner not registered.',
+            'data' => []
+        ];
+        $statusCode = 406;
+
         $newHiringPartnerData = $request->getParsedBody();
 
-        $postcodeRegex = '#^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})$#';
-
-        $companyName = $newHiringPartnerData['companyName'];
-        $size = $newHiringPartnerData['size'];
-        $techStack = $newHiringPartnerData['techStack'];
-        $postcode = $newHiringPartnerData['postcode'];
-        $phoneNo = $newHiringPartnerData['phoneNo'] ?? 'not known'; //null coalesce
-        $url = $newHiringPartnerData['url'];
-
-        if (
-            !empty($companyName) &&
-            is_string($companyName) &&
-            !empty($size) &&
-            is_int($size) &&
-            !empty($techStack) &&
-            is_string($techStack) &&
-            !empty($postcode) &&
-            preg_match($postcodeRegex, $postcode) // need to add validation for phoneNo and url here
-        ) {
-            $validatedNewHiringPartnerData = [
-                'companyName' => $companyName,
-                'size' => $size,
-                'techStack' => $techStack,
-                'postcode' => $postcode,
-                'phoneNo' => filter_var($phoneNo, FILTER_SANITIZE_STRING), //need to validate this
-                'url' => filter_var($url, FILTER_SANITIZE_URL)
-            ];
-
-            $successfulNewHiringPartner = $this->hiringPartnerModel->insertNewHiringPartnerToDb(
-                $validatedNewHiringPartnerData['companyName'],
-                $validatedNewHiringPartnerData['size'],
-                $validatedNewHiringPartnerData['techStack'],
-                $validatedNewHiringPartnerData['postcode'],
-                $validatedNewHiringPartnerData['phoneNo'],
-                $validatedNewHiringPartnerData['url']
-                );
+        try {
+            $hiringPartnerEntity = new HiringPartnerEntity(
+                $newHiringPartnerData['companyName'],
+                $newHiringPartnerData['size'],
+                $newHiringPartnerData['techStack'],
+                $newHiringPartnerData['postcode'],
+                $newHiringPartnerData['phoneNo'],
+                $newHiringPartnerData['url']
+            );
+        } catch (\Error $e) {
+            return $response->withJson($data, $statusCode);
         }
+
+        $successfulNewHiringPartner = $this->hiringPartnerModel->insertNewHiringPartnerToDb($hiringPartnerEntity);
+
+        if ($successfulNewHiringPartner) {
+            $data = [
+                'success' => $successfulNewHiringPartner,
+                'msg' => 'New hiring partner ' . $newHiringPartnerData['companyName'] . ' added to the database',
+                'data' => []
+            ];
+            $statusCode = 200;
+        }
+        return $response->withJson($data, $statusCode);
     }
 }
