104ff3245a3cdbb0f924bc731004bb34fadf4735
Felix <felixgate13@gmail.com>
2018-11-28 15:51:13 +0000

    coded the view helper to dynamically display data form DB

--- src/Classes/ViewHelpers/EventListViewHelper.php
+++ src/Classes/ViewHelpers/EventListViewHelper.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: academy
- * Date: 28/11/2018
- * Time: 14:26
- */
 
 namespace Portal\ViewHelpers;
 
@@ -13,8 +7,52 @@ class EventListViewHelper
 {
     public static function outputEventList($events) {
         foreach ($events as $event) {
+            $return =  '<div class= '. "event" . '>' .
+            '<div class=' . "top" . '>' .
+                '<div class=' . "upper" . '>' .
+                    '<div class=' . "infoBox" . '>' .
+                        '<span><strong>Event Name</strong></span>' .
+                        '<span>' . $event['eventName'] . '</span>' .
+                    '</div>' .
+                    '<div class=' . "infoBox" . '>' .
+                        '<span><strong>Type</strong></span>' .
+                        '<span >' . $event['type']  . '</span>' .
+                    '</div>
+                    <div class=' . "infoBox" . '>
+                        <span><strong>Date</strong></span>' .
+                        '<span >' . $event['date'] . '</span>' .
+                    '</div>
+                </div>
 
+                <div class=' . "mid" . '>
+                    <div class=' . "infoBox-mid" . '>
+                        <span><strong>Start Time</strong></span>' .
+                '<span >' . $event['startTime'] . '</span>' .
+                    '</div>
+                    <div class= ' . "infoBox-mid" . '>
+                        <span><strong>End Time</strong></span>' .
+                '<span >' . $event['endTime'] . '</span>' .
+                    '</div>
+                </div>
+            </div>
+            <div class=' . "arrowBox" . '>
+                <span>&#x21E9;</span>
+            </div>
+            <div class=' . "lower" . '>
+                <div class=' . "infoBox-low" . '>
+                    <span><strong>Location</strong></span>
+                <div>' . $event['location'] . '</div>
+                </div>
+                <div class=' . "infoBox-low" . '>
+                    <span><strong>Notes</strong></span>
+                    <div>' . $event['notes'] . '</div>
+                </div>
+            </div>
+
+        </div>';
+            $arr[] = $return;
         }
+        return $arr;
 
     }
 
