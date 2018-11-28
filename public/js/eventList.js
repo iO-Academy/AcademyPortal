cb6f51ea42c4a7de4e1b2715a876cf3d880aae65
Felix <felixgate13@gmail.com>
2018-11-28 16:33:25 +0000

    deleted whitespace and made the quotes constant

--- public/js/eventList.js
+++ public/js/eventList.js
@@ -4,30 +4,32 @@ document.querySelectorAll(".event .lower").forEach(function(event) {
     event.style.height = 0
 })
 
-document.querySelectorAll(".event").forEach(function(event) {
+document.querySelectorAll('.event').forEach(function(event) {
     let clicked = 0
     event.addEventListener('click', function() {
         clicked++
         let lower = this.querySelector('.lower')
-        let arrow = this.querySelector(".arrowBox")
+        let arrow = this.querySelector('.arrowBox')
         if(clicked % 2) {
-            arrow.innerHTML = "<span>&#x21E7;</span>"
+            arrow.innerHTML = '<span>&#x21E7;</span>'
 
             var id = setInterval(function() {
-                if (lower.clientHeight < lower.dataset.height) {
-                    lower.style.height = (parseInt(lower.style.height) + 15) + 'px'
+                if (lower.clientHeight < (parseInt(lower.dataset.height))) {
+                    lower.style.height = (parseInt(lower.style.height) + 10) + 'px'
                 } else {
                     clearInterval(id)
                 }
+
             }, 16)
         }
         else {
-            arrow.innerHTML = "<span>&#x21E9;</span>"
+            arrow.innerHTML = '<span>&#x21E9;</span>'
 
             var id = setInterval(function() {
                 if (lower.clientHeight > 0) {
                     lower.style.height = (parseInt(lower.style.height) - 10) + 'px'
-                } else {
+                }
+                else {
                     clearInterval(id)
                 }
             }, 16)
