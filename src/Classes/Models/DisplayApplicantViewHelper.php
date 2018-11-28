fb572cea4d1741fabf16d48a7c98d742b81c71ee
Daniel Coombes <daniel.coombes224@gmail.com>
2018-11-28 13:22:43 +0000

    Doc blocked dislayApplicantViewhELPER

--- src/Classes/Models/DisplayApplicantViewHelper.php
+++ src/Classes/Models/DisplayApplicantViewHelper.php
@@ -6,6 +6,13 @@ namespace Portal\Models;
 class DisplayApplicantViewHelper
 {
 
+    /**
+     * Get's applicants name, email and cohortID, to display.
+     *
+     * @param $applicants.
+     *
+     * @return string for name, email and cohortID.
+     */
     static public function displayApplicants($applicants)
     {
         $result = '';
