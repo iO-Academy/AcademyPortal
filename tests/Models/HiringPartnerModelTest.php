82cc9d099d3e9dedc73149ec218f149bffe4f9cb
rrrichard <richard.rabulan@hotmail.co.uk>
2019-05-14 11:56:49 +0100

    created a mock test for the HiringPartnetModel

---
+++ tests/Models/HiringPartnerModelTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Models;
+
+use PHPUnit\Framework\TestCase;
+use Portal\Models\HiringPartnerModel;
+
+class HiringPartnerModelTest extends TestCase
+{
+    function testConstruct()
+    {
+        $db = $this->createMock(\PDO::class);
+        $case = new HiringPartnerModel($db);
+        $expected = HiringPartnerModel::class;
+        $this->assertInstanceOf($expected, $case);
+    }
+}
