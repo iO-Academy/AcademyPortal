6b6453289ff506437af8c9e483da089bf848ee08
fredplatt <fred_platt@hotmail.com>
2019-05-14 13:44:36 +0100

    test written following template of HomePageControllerTest

--- tests/Controllers/DisplayHiringPartnerPageControllerTest.php
+++ tests/Controllers/DisplayHiringPartnerPageControllerTest.php
@@ -6,33 +6,12 @@ use Portal\Models\HiringPartnerModel;
 
 class DisplayHiringPartnerPageControllerTest extends TestCase {
 
-    public function testSuccess()
+    function testConstruct()
     {
-        $dog = $this->createMock(Portal\Classes\Dog::class); //blank pretend object
-        $dog->method('getUrl')
-            ->willReturn('www.dog.com');
-        $inputDogs = [$dog];
-        $dogDisplayer = new \TopDog\Classes\DogDisplayer();
-        $result = $dogDisplayer->displayDogs($inputDogs);
-        $stringResult = '<div class="dog-holder"><form action="post"><div class="dog-image"><img src="www.dog.com" alt="doggy"></div><input type="hidden" id="favDogId" name="favDogId" value="0"><input type="submit" value="Make favourite!"></form></div>';
-        $this->assertEquals($result['dogs'], $stringResult);
-    }
+        $stub = $this->createMock(PhpRenderer::class);
 
-    public function testFailure()
-    {
-        $dog = $this->createMock(Portal\Classes\Dog::class); //blank pretend object
-        $dog->method('getUrl')
-            ->willReturn('');
-        $inputDogs = [$dog];
-        $dogDisplayer = new \TopDog\Classes\DogDisplayer();
-        $result = $dogDisplayer->displayDogs($inputDogs);
-        $stringResult = '<div class="dog-holder"><form action="post"><div class="dog-image"><img src="" alt="doggy"></div><input type="hidden" id="favDogId" name="favDogId" value="0"><input type="submit" value="Make favourite!"></form></div>';
-        $this->assertEquals($result['dogs'], $stringResult);
+        $case = new DisplayHiringPartnerPageController($stub);
+        $expected = DisplayHiringPartnerPageController::class;
+        $this->assertInstanceOf($expected, $case);
     }
-
-    public function testMalformed()
-    {
-        $dogDisplayer = new \Portal\Classes\DogDisplayer();
-        $this->expectException(TypeError::class);
-        $dogDisplayer->displayDogs(7);
 }
