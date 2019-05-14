a003fd742be8864c840f18cbc14f78fc41ab2f4c
Knux2 <ben_ashfield@hotmail.com>
2019-05-14 14:02:51 +0100

    Added construtor to GetCompanySizeController

--- src/Classes/Controllers/GetCompanySizeController.php
+++ src/Classes/Controllers/GetCompanySizeController.php
@@ -9,7 +9,23 @@
 namespace Portal\Controllers;
 
 
+use Portal\Models\HiringPartnerModel;
+
 class GetCompanySizeController
 {
+    private $renderer;
+    private $hiringPartnerModel;
 
+    /**
+     * DisplayApplicantsController constructor.
+     *
+     * @param PhpRenderer $renderer
+     *
+     * @param HiringPartnerModel $hiringPartnerModel
+     */
+    public function __construct(PhpRenderer $renderer, HiringPartnerModel $hiringPartnerModel)
+    {
+        $this->renderer = $renderer;
+        $this->hiringPartnerModel = $hiringPartnerModel;
+    }
 }
