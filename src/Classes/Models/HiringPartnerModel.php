f5627c22c0b0e290f32bc603df034b1fa0b683cf
rrrichard <richard.rabulan@hotmail.co.uk>
2019-05-14 11:43:59 +0100

    modified addHiringPartner to take on entities

--- src/Classes/Models/HiringPartnerModel.php
+++ src/Classes/Models/HiringPartnerModel.php
@@ -11,16 +11,37 @@ class HiringPartnerModel {
         $this->db = $db;
     }
 
-    public function addHiringPartner($newCompanyName) {
-        $query = $this->db->prepare("INSERT INTO `hiring_partner_companies` (`company_name`) VALUES `:newCompanyName`;");
-        $query->bindParam(':newCompanyName', $newCompanyName);
+    //Remember to typhint the HiringPartnerEntity in the param
+    public function addHiringPartner($company) {
+        $query = $this->db->prepare("INSERT INTO `hiring_partner_companies` (
+            `company_name`,
+            `size`, 
+            `tech_stack`,
+            `postcode`,
+            `phone_number`,
+            `url_website`
+            )
+            VALUES (
+            `:companyName`,
+            `:companySize`,
+            `:techStack`,
+            `:postcode`,
+            `:phoneNumber`,
+            `:websiteUrl`
+            );"
+        );
+        $query->bindParam(':companyName', $company->getCompanyName);
+        $query->bindParam(':companySize', $company->getCompanySize);
+        $query->bindParam(':techStack', $company->getTechStack);
+        $query->bindParam(':postcode', $company->getPostcode);
+        $query->bindParam(':phoneNumber', $company->getPhoneNumber);
+        $query->bindParam(':websiteUrl', $company->getWebsiteUrl);
         return $query->execute();
     }
 
-    public function getCompanySize($companySize, $companyId) {
-        $query = $this->db->prepare("INSERT INTO `company_sizes` (`company_size`) VALUES :companySize WHERE `id` = :companyId; ");
-        $query->bindParam(':companySize', $companySize);
-        $query->bindParam(':companyId', $companyId);
-        return $query->execute();
+    public function getCompanySize() {
+        $query = $this->db->prepare("SELECT `id`,`size` FROM `company_sizes`");
+        $query->execute();
+        return $query->fetchAll();
     }
 }
