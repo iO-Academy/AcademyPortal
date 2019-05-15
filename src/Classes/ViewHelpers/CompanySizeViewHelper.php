99102716e3729ccafdaad654f8221fb42e50f91b
katestarks <katestarks@me.com>
2019-05-15 12:02:10 +0100

    added function companySizeDropdown

--- src/Classes/ViewHelpers/CompanySizeViewHelper.php
+++ src/Classes/ViewHelpers/CompanySizeViewHelper.php
@@ -1,3 +1,13 @@
 <?php
 
-namespace Portal
+namespace Portal\ViewHelpers;
+
+class CompanySizeViewHelper
+{
+    static public function companySizeDropdown($companySizes) {
+        $companySizeDropdown = '';
+        foreach ($companySizes as $companySize) {
+            $companySizeDropdown .= '<option value=' . $companySize['id'] . '>' . $companySize['company_size'] . '</option>';
+        }
+    }
+}
