d80faf3f149a1624a2df9e8281c2b63cb0b45316
rrrichard <richard.rabulan@hotmail.co.uk>
2019-05-15 12:57:51 +0100

    made the test simple by making sure the factory is what is being outputted

--- tests/Factories/HiringPartnerControllerFactoryTest.php
+++ tests/Factories/HiringPartnerControllerFactoryTest.php
@@ -2,22 +2,13 @@
 
 use PHPUnit\Framework\TestCase;
 use Portal\Factories\HiringPartnerControllerFactory;
-use Portal\Controllers\HiringPartnerController;
-use Slim\Views\PhpRenderer;
-use Psr\Container\ContainerInterface;
 
 class HiringPartnerControllerFactoryTest extends TestCase
 {
     public function testInvoke()
     {
-        $container = $this->createMock(ContainerInterface::class);
-        $renderer = $this->createMock(PhpRenderer::class);
-        $container->method('get')
-            ->willReturn($renderer);
-
-        $factory = new HiringPartnerControllerFactory;
-        $case = $factory($container);
-        $expected = HiringPartnerController::class;
-        $this->assertInstanceOf($expected, $case);
+        $factory = new HiringPartnerControllerFactory();
+        $expected = HiringPartnerControllerFactory::class;
+        $this->assertInstanceOf($expected, $factory);
     }
 }
