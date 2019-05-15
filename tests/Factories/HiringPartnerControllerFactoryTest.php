9934679c80401761f0aba86fc6cba5fc26abbb03
rrrichard <richard.rabulan@hotmail.co.uk>
2019-05-15 10:37:30 +0100

    made sure the naming matches with the HiringPartnerController

---
+++ tests/Factories/HiringPartnerControllerFactoryTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+
+class HiringPartnerControllerFactoryTest extends TestCase
+{
+    public function testInvoke()
+    {
+        $container = $this
+    }
+}
