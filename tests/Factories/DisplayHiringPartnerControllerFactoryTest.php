a2e52ce2b87a4f196337bd971398f0c5b5368cc2
Jkapella <jvkapella@gmail.com>
2019-05-15 11:38:17 +0100

    corrected bug in test

--- tests/Factories/DisplayHiringPartnerControllerFactoryTest.php
+++ tests/Factories/DisplayHiringPartnerControllerFactoryTest.php
@@ -1,17 +1,15 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use Psr\Container\ContainerInterface;
-use Portal\Controllers\DisplayHiringPartnerPageController;
 
 class DisplayHiringPartnerPageControllerFactoryTest extends TestCase {
 
     function testDisplayHiringPartnerPageControllerFactory()
     {
-        $container = $this->createMock(ContainerInterface::class);
-        $factory = new \Portal\Factories\DisplayHiringPartnerPageControllerFactory();
-        $case = $factory($container);
-        $expected = DisplayHiringPartnerPageController::class;
+        $renderer = $this->createMock(\Slim\Views\PhpRenderer::class);
+        $model = $this->createMock(\Portal\Models\HiringPartnerModel::class);
+        $case = new \Portal\Controllers\DisplayHiringPartnerPageController($renderer, $model);
+        $expected = \Portal\Controllers\DisplayHiringPartnerPageController::class;
         $this->assertInstanceOf($expected, $case);
     }
 }
