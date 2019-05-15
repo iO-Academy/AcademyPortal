05f75f5478b0ed390a86ed6962b67a91280c4355
Knux2 <ben_ashfield@hotmail.com>
2019-05-15 14:10:49 +0100

    Saved work done thus far

--- public/js/hiringPartnerValidation.js
+++ public/js/hiringPartnerValidation.js
@@ -1,20 +1,63 @@
+//
+// document.querySelector("#submitHiringPartner").addEventListener('submit', function () {
 
-document.getElementById('formButton').addEventListener('click', function () {
+
+document.getElementById('hpFrom').addEventListener('submit', function (e) {
+e.preventDefault()
     var message = ''
 
-   var inputs = document.querySelectorAll('input')
+    var inputs = document.querySelectorAll('input')
 
-    inputs.forEach(function (el) {
-        if(el.data('min')) {
-            var min = el.data('min')
-            if (
-                (el.length < min && el.data('required') == 'true')
-                ||
-                (el.length < min && el.length > 0 && !el.data('required') != 'true')
-            ) {
-                message += min + 'character minimum <br>'
+    for (i = 0; i < inputs.length; i++)
+    {
+        if(inputs[i].getAttribute('data-min')){
+            var min = inputs[i].getAttribute('data-min')
+            if (inputs[i].value.length < min && inputs[i].getAttribute('data-required') === 'true') {
+                message += min + ' character minimum <br>'
             }
         }
-    })
+    }
+    console.log(message)
+    document.getElementById('message').innerHTML = message
 })
+
+let isValidCompanyName = (inputEmail) => {
+    let email = inputEmail.trim()
+    let regEx = new RegExp("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$")
+    return regEx.test(email)
+}
+
+
+// })
+
+    //
+
+    //
+    // console.log(this.getAttribute('data-required'))
+    // if(this.data('min')) {
+    //     var min = this.data('min')
+    //     if (
+    //         (this.length < min && this.data('required') === 'true')
+    //         ||
+    //         (this.length < min && this.length > 0 && !this.data('required') !== 'true')
+    //     ) {
+    //         message += min + 'character minimum <br>'
+    //     }
+    // }
+
+
+
+
+
+
+
+
+
+// console.log(document.querySelector("#formButton"))
+
+// inputs[i].getAttribute('data-required') === 'true'
+
+// var showMessage = function($el, message) {
+//     var alert = "<div class='error'>" + message + "</div>"
+//     $el.after(alert);
 
