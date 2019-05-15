9f0a740df36ab4b6db98582b8df0f77dea38bfd6
rrrichard <richard.rabulan@hotmail.co.uk>
2019-05-15 10:55:34 +0100

    added a mock test for the HiringPartnerControllerFactory

--- tests/Factories/HiringPartnerControllerFactoryTest.php
+++ tests/Factories/HiringPartnerControllerFactoryTest.php
@@ -1,12 +1,23 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-
+use Portal\Factories\HiringPartnerControllerFactory;
+use Portal\Controllers\HiringPartnerController;
+use Slim\Views\PhpRenderer;
+use Psr\Container\ContainerInterface;
 
 class HiringPartnerControllerFactoryTest extends TestCase
 {
     public function testInvoke()
     {
-        $container = $this
+        $container = $this->createMock(ContainerInterface::class);
+        $renderer = $this->createMock(PhpRenderer::class);
+        $container->method('get')
+            ->willReturn($renderer);
+
+        $factory = new HiringPartnerControllerFactory();
+        $case = $factory($container);
+        $expected = HiringPartnerController::class;
+        $this->assertInstanceOf($expected, $case);
     }
 }
