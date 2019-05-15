0f1a8e30bc5ea287010382673219e11f78a15526
rrrichard <richard.rabulan@hotmail.co.uk>
2019-05-15 10:59:53 +0100

    added docblocks

--- src/Classes/Factories/HiringPartnerControllerFactory.php
+++ src/Classes/Factories/HiringPartnerControllerFactory.php
@@ -7,6 +7,11 @@ use Psr\Container\ContainerInterface;
 
 class HiringPartnerControllerFactory
 {
+    /**Controller for adding hiring partners to the database
+     *
+     * @param ContainerInterface $container
+     * @return HiringPartnerController instantiated with both params from the container
+     */
     public function __invoke(ContainerInterface $container)
     {
         $renderer = $container->get('renderer');
