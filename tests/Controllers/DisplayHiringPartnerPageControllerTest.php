429e599076d1d9b0514dc2bed5b31ce4cd5035ee
fredplatt <fred_platt@hotmail.com>
2019-05-15 10:55:56 +0100

    test written

--- tests/Controllers/DisplayHiringPartnerPageControllerTest.php
+++ tests/Controllers/DisplayHiringPartnerPageControllerTest.php
@@ -4,21 +4,14 @@ use PHPUnit\Framework\TestCase;
 use Portal\Controllers\DisplayHiringPartnerPageController;
 use Portal\Models\HiringPartnerModel;
 
-class DisplayHiringPartnerPageControllerTest extends TestCase {
+class DisplayHiringPartnerPageControllerTest extends TestCase
+{
 
     public function testConstruct()
     {
         $mockRender = $this->createMock(PhpRenderer::class);
         $mockModel = $this->createMock(HiringPartnerModel::class);
         $case = new DisplayHiringPartnerPageController($mockRender, $mockModel);
-        $expected = DisplayHiringPartnerPageController::class;
-        $this->assertInstanceOf($expected, $case);
-    }
-
-    public function testGetCompanyInfo()
-    {
-        $mock = $this->createMock(HiringPartnerModel::class);
-        $case = new DisplayHiringPartnerPageController($mock);
         $expected = DisplayHiringPartnerPageController::class;
         $this->assertInstanceOf($expected, $case);
     }
