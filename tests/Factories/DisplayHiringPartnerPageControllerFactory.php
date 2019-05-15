280d62e7c406b66eea8cfb690e11ba8d07493122
katestarks <katestarks@me.com>
2019-05-15 10:33:10 +0100

    added constructor test

--- tests/Factories/DisplayHiringPartnerPageControllerFactory.php
+++ tests/Factories/DisplayHiringPartnerPageControllerFactory.php
@@ -1,2 +1,16 @@
 <?php
 
+use PHPUnit\Framework\TestCase;
+use Portal\Controllers\DisplayHiringPartnerPageController;
+use Portal\Models\HiringPartnerModel;
+
+class DisplayHiringPartnerPageControllerFactoryTest extends TestCase {
+
+    function testConstruct()
+    {
+        $stub = $this->createMock(PhpRenderer::class);
+        $case = new DisplayHiringPartnerPageControllerFactory($stub);
+        $expected = DisplayHiringPartnerPageControllerFactory::class;
+        $this->assertInstanceOf($expected, $case);
+    }
+}
