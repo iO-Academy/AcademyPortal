fcec9b444c0687eb2544c732e591a713cffd1b12
Liliya Voevodina <liliiavoevodina@gmail.com>
2019-05-15 14:15:24 +0100

    applied some CSS to a display registered applicants

--- src/Classes/ViewHelpers/DisplayApplicantViewHelper.php
+++ src/Classes/ViewHelpers/DisplayApplicantViewHelper.php
@@ -18,13 +18,9 @@ class DisplayApplicantViewHelper
         $result = '';
         foreach ($applicants as $applicant) {
             if ($applicant instanceof ApplicantEntity) {
-<<<<<<< HEAD
-                $result .= '<tr>
-                        <td><a data-id ="'. $applicant->getId().'" type="button"  class="myBtn">'. $applicant->getName() .'</a></td>
-=======
+
                 $result .= '<tr class="applicant">
                         <td>'. $applicant->getName() .'</td>
->>>>>>> RMPstory2_task_date-filter
                         <td>'. $applicant->getEmail() .'</td>
                         <td class="dateApplied" data-applied="'. $applicant->getDateOfApplicationUsingMMDDYYYY() .'">'. $applicant->getDateOfApplication() .'</td>
                         <td>'. $applicant->getCohortDate() .'</td>
