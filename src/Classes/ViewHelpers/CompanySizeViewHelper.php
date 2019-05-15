c645b99ea95ce6d1fa2f55148aaf661451403204
Jkapella <jvkapella@gmail.com>
2019-05-15 13:01:58 +0100

    removed erroneous var dump

--- src/Classes/ViewHelpers/CompanySizeViewHelper.php
+++ src/Classes/ViewHelpers/CompanySizeViewHelper.php
@@ -5,7 +5,6 @@ namespace Portal\ViewHelpers;
 class CompanySizeViewHelper
 {
     static public function companySizeDropdown($data) {
-        var_dump($data);
         $companySizeDropdown = '';
         foreach ($data['companySize'] as $companySize) {
             $companySizeDropdown .= '<option value=' . $companySize['id'] . '>' . $companySize['size'] . '</option>';
