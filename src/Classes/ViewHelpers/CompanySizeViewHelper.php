76e8627b14479ff45b8db00d0003fb097499213e
Jkapella <jvkapella@gmail.com>
2019-05-15 12:57:25 +0100

    fixed viewhelper and added formatting to form

--- src/Classes/ViewHelpers/CompanySizeViewHelper.php
+++ src/Classes/ViewHelpers/CompanySizeViewHelper.php
@@ -5,9 +5,11 @@ namespace Portal\ViewHelpers;
 class CompanySizeViewHelper
 {
     static public function companySizeDropdown($data) {
+        var_dump($data);
         $companySizeDropdown = '';
         foreach ($data['companySize'] as $companySize) {
             $companySizeDropdown .= '<option value=' . $companySize['id'] . '>' . $companySize['size'] . '</option>';
         }
+        return $companySizeDropdown;
     }
 }
