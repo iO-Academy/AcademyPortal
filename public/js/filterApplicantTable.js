d9c83c49cf756c6cb651ed823434b8bf2d47bcfd
Oliver Matthews <o.matthews@rocketmail.com>
2019-05-15 13:42:09 +0100

    made the date in applicaticant date current and fixed apply button

--- public/js/filterApplicantTable.js
+++ public/js/filterApplicantTable.js
@@ -1,19 +1,21 @@
 function filterByDate (dateFrom, dateTo){
-    document.querySelectorAll('#applicants .applicant').forEach(row => {
+    document.querySelector('.btn.apply').addEventListener('click',function(){
+        document.querySelectorAll('#applicants .applicant').forEach(row => {
 
-        var dateCheck = row.querySelector('.dateApplied').dataset.applied;
+            var dateCheck = row.querySelector('.dateApplied').dataset.applied;
 
-        //console.log(dateCheck)
+            //console.log(dateCheck)
 
-        var applicationDateAsArray = dateCheck.split("/");
+            var applicationDateAsArray = dateCheck.split("/");
 
-        var applicationDate = new Date(applicationDateAsArray[2], (applicationDateAsArray[0])-1, applicationDateAsArray[1]) // -1 because January = 0, December = 11
+            var applicationDate = new Date(applicationDateAsArray[2], (applicationDateAsArray[0])-1, applicationDateAsArray[1]) // -1 because January = 0, December = 11
 
-        if (applicationDate >= dateFrom && applicationDate <= dateTo) {
-        } else {
-            row.classList += " hidden"
-        }
+            if (applicationDate >= dateFrom && applicationDate <= dateTo) {
+                row.classList.remove('hidden')
+            } else {
+                row.classList.add('hidden')
+            }
+        })
     })
-
 }
 
