b50d2932a81eb15e9a37d71b1e191c145a1e3ccd
Knux2 <ben_ashfield@hotmail.com>
2019-05-15 09:06:11 +0100

    Added DocBlock to Controller Factory

--- src/Classes/Factories/GetCompanySizeControllerFactory.php
+++ src/Classes/Factories/GetCompanySizeControllerFactory.php
@@ -7,6 +7,14 @@ use Portal\Controllers\GetCompanySizeController;
 
 class GetCompanySizeControllerFactory
 {
+
+    /**
+     * Creates Company Size Controller with dependencies
+     *
+     * @param ContainerInterface $container DIC
+     *
+     * @return GetCompanySizeController returns object with dependency on Hiring Partner Model
+     */
     public function __invoke(ContainerInterface $container)
     {
         $hiringPartnerModel = $container->get('HiringPartnerModel');
