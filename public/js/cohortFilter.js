d9b28b40dc3ae8be05c3248ce254265a0c2777b9
Alessandro Corra <alecorra1980@gmail.com>
2019-05-16 10:24:15 +0100

    adedd dock blocks

--- public/js/cohortFilter.js
+++ public/js/cohortFilter.js
@@ -1,6 +1,11 @@
 let tableRows = document.querySelectorAll('.table-row')
 let applicantsDates = document.querySelectorAll('.applicants-cohort-date')
 
+/**
+ * filter through nodelist of cohort date items to hide mismatches
+ *
+ * @param cohortDate
+ */
 function applyCohortFilter(cohortDate) {
     let mismatches = [];
     applicantsDates.forEach(function (date, index) {
@@ -14,6 +19,9 @@ function applyCohortFilter(cohortDate) {
     })
 }
 
+/**
+ *   resets all the hidden rows
+ */
 function clearFilter() {
     tableRows.forEach(function(tableRow) {
         if(tableRow.classList.contains('hidden')){
@@ -22,6 +30,11 @@ function clearFilter() {
     })
 }
 
+/**
+ * makes dropdown select cohortDate and calls the filter function
+ *
+ * @return cohortDate (yyyy/mm/dd)
+ */
 $('.cohort-item').click(function () {
     let cohortDate = this.dataset.cohortdate
     let cohortDateNice = this.textContent
