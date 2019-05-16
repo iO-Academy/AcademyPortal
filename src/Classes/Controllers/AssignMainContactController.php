62651d5967c73143f8147adce046b6c590849ed9
Jkapella <jvkapella@gmail.com>
2019-05-16 16:18:52 +0100

    completed invoke method for assign main contact controller

--- src/Classes/Controllers/AssignMainContactController.php
+++ src/Classes/Controllers/AssignMainContactController.php
@@ -19,6 +19,30 @@ class AssignMainContactController
 
     public function __invoke(Request $request, Response $response, array $args)
     {
-        // TODO: Implement __invoke() method.
+        $newMainContact = $request->getParsedBody();
+
+        $data = [
+            'success' => false,
+            'message' => 'Unable to set new main contact!',
+            'data' => []
+        ];
+        $statusCode = 400;
+
+        try {
+            $result = $this->contactsModel->makeHiringPartnerContactMain($newMainContact['contactId'], $newMainContact['companyId']);
+        } catch (\Exception $exception) {
+            $data['message'] = $exception->getMessage();
+        }
+
+        if (isset($result) && $result) {
+            $data = [
+                'success' => true,
+                'message' => 'new main contact set!',
+                'data' => []
+            ];
+            $statusCode = 200;
+        }
+
+        return $response->withJson($data, $statusCode);
     }
 }
