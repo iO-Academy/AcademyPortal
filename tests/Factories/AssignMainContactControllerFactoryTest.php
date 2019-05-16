0f823ab5f016224b2b5de0a3e6f3814d66fef41c
katestarks <katestarks@me.com>
2019-05-16 16:33:18 +0100

    updated test with namespacing

--- tests/Factories/AssignMainContactControllerFactoryTest.php
+++ tests/Factories/AssignMainContactControllerFactoryTest.php
@@ -2,17 +2,19 @@
 
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
+use Portal\Models\HiringPartnerContactsModel;
+use Portal\Factories\AssignMainContactControllerFactory;
 
 class AssignMainContactControllerFactoryTest extends TestCase {
 
     function testAssignMainContactControllerFactory()
     {
         $container = $this->createMock(ContainerInterface::class);
-        $hiringPartnerContactsModel = $this->createMock(\Portal\Models\HiringPartnerContactsModel::class);
+        $hiringPartnerContactsModel = $this->createMock(HiringPartnerContactsModel::class);
         $container->method('get')->willReturn($hiringPartnerContactsModel);
-        $factory = new \Portal\Factories\AssignMainContactControllerFactory();
+        $factory = new AssignMainContactControllerFactory();
         $case = $factory($container);
-        $expected = \Portal\Factories\AssignMainContactControllerFactory::class;
+        $expected = AssignMainContactControllerFactory::class;
         $this->assertInstanceOf($expected, $case);
     }
 }
