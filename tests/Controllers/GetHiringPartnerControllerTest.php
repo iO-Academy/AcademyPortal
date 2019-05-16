5d21907d7ffe87744b62bfbf2da3711286aa0bd6
aldofiore28 <aldo.fiore95@gmail.com>
2019-05-16 10:32:53 +0100

    added basic test

---
+++ tests/Controllers/GetHiringPartnerControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Portal\Controllers\HomePageController;
+use Slim\Views\PhpRenderer;
+
+class GetHiringPartnerControllerTest extends TestCase
+{
+    function testConstruct()
+    {
+
+    }
+}
