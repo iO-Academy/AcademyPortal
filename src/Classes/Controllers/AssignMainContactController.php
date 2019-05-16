44071678e57bcc839b0c6af13a9e97d66275e2ae
Jkapella <jvkapella@gmail.com>
2019-05-16 16:05:35 +0100

    added new class for assign main contact controller

---
+++ src/Classes/Controllers/AssignMainContactController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Portal\Controllers;
+
+
+use Portal\Models\HiringPartnerContactsModel;
+
+class AssignMainContactController
+{
+    public $contactsModel;
+
+    public function __construct(HiringPartnerContactsModel $hiringPartnerContactsModel)
+    {
+
+    }
+}
