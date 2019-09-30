7906261308c2c34b47eaf4b450a632cbfcf8c76f
Liz <ejgsharp@gmail.com>
2019-09-30 14:26:43 +0100

    Added GetEventsInfoControllerFactory to the dic. Added request/response method to the GetEventsController file

--- src/Classes/Controllers/GetEventsInfoController.php
+++ src/Classes/Controllers/GetEventsInfoController.php
@@ -4,6 +4,9 @@
 namespace Portal\Controllers;
 
 
+use Slim\Http\Request;
+use Slim\Http\Response;
+
 class GetEventsInfoController
 {
     private $eventsModel;
@@ -11,6 +14,33 @@ class GetEventsInfoController
     public function __construct($eventsModel)
     {
         $this->eventsModel = $eventsModel;
+    }
+
+    public function __invoke(Request $request, Response $response, array $args)
+    {
+        $data = [
+            'status' => false,
+            'message' => 'No events found!',
+            'data' => []
+        ];
+        $statusCode = 400;
+
+        try {
+            $events = $this->eventsModel->getEvents();
+        } catch (\PDOException $exception) {
+            $data['message'] = $exception->getMessage();
+        }
+
+        if (!empty($events)) {
+            $data = [
+                'status' => true,
+                'message' => 'Query Successful',
+                'data' => $events
+            ];
+            $statusCode = 200;
+        }
+
+        return $response->withJson($data, $statusCode);
     }
 
 }
