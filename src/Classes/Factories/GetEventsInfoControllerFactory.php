93827e0c4bed8cb70a35c45faf172f09f2beaea1
Liz <ejgsharp@gmail.com>
2019-09-30 14:15:45 +0100

    Added GetEventsInfoControllerFactory

--- src/Classes/Factories/GetEventsInfoControllerFactory.php
+++ src/Classes/Factories/GetEventsInfoControllerFactory.php
@@ -4,7 +4,14 @@
 namespace Portal\Factories;
 
 
+use Portal\Controllers\GetEventsInfoController;
+use Psr\Container\ContainerInterface;
+
 class GetEventsInfoControllerFactory
 {
-
+    public function __invoke(ContainerInterface $container)
+{
+    $eventsModel = $container->get('EventsModel');
+    return new GetEventsInfoController($eventsModel);
+}
 }
