3340f3b2fab53450c85b42040482d83280eef0e8
Felix <felix.letheren@gmail.com>
2019-10-01 11:07:19 +0100

    Removed unneeded rejex.

--- public/js/newTaskValidation.js
+++ public/js/newTaskValidation.js
@@ -31,37 +31,7 @@ function validateForm() {
             message += 'Please select a company size!<br>'
             success = false
         }
-
-        if (element.name === 'postcode') {
-            let postcode =  element.value.trim()
-            let pattern = /\b((?:(?:gir)|(?:[a-pr-uwyz])(?:(?:[0-9](?:[a-hjkpstuw]|[0-9])?)|(?:[a-hk-y][0-9](?:[0-9]|[abehmnprv-y])?)))) ?([0-9][abd-hjlnp-uw-z]{2})\b/ig
-            let regEx = new RegExp(pattern)
-            if (!regEx.test(postcode)) {
-                message += 'Invalid postcode!<br>'
-                success = false
-            }
-        }
-
-        if (element.name === 'phoneNumber' && element.value.length > 0) {
-            let phoneNumber =  element.value.trim()
-            let pattern = /^(([+][(]?[0-9]{1,3}[)]?)|([(]?[0-9]{4}[)]?))\s*[)]?[-\s\.]?[(]?[0-9]{1,3}[)]?([-\s\.]?[0-9]{3})([-\s\.]?[0-9]{3,4})$/gm
-            let regEx = new RegExp(pattern)
-            if (!regEx.test(phoneNumber)) {
-                message += 'Invalid phone number format!<br>'
-                success = false
-            }
-        }
-
-        if (element.name === 'companyUrl' && element.value.length > 0) {
-            let url =  element.value.trim()
-            let pattern = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm
-            let regEx = new RegExp(pattern)
-            if (!regEx.test(url)) {
-                message += 'Invalid URL format!<br>'
-                success = false
-            }
-        }
-
+        
         if (element.name === 'companySize') {
             let idRange = document.getElementsByTagName('option').length -1
             if (element.value > idRange) {
