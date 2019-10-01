3e846679d7ecf8b0ef95b2d0f707acb7c4073cb5
Cat <blackcatstone@gmail.com>
2019-10-01 09:30:04 +0100

    Test Event Model constructor

---
+++ tests/Models/EventModelTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Models;
+
+use PHPUnit\Framework\TestCase;
+use Portal\Models\EventModel;
+
+class EventModelTest extends TestCase
+{
+    public function testConstruct()
+    {
+        $db = $this->createMock(\PDO::class);
+        $case = new EventModel($db);
+        $expected = EventModel::class;
+
+        $this->assertInstanceOf($expected, $case);
+    }
+}
