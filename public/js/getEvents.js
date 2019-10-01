737484503232dc6b247bbc5ef928066bafe95425
Rachmann <rachmann.joubert@gmail.com>
2019-10-01 13:21:04 +0100

    added get events function

--- public/js/getEvents.js
+++ public/js/getEvents.js
@@ -1,22 +1,11 @@
-let makeEventApiRequest = async(data) => {
-    return fetch('/api/createHiringPartner', { // ADD STUFF LATER
-        credentials: 'same-origin',
+async function getEvents () {
+    await fetch('/api/getEvents', {
+        credentials: "same-origin",
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
         },
-        method: 'post',
-        body: JSON.stringify(data)
     })
-        .then(response => response.json())
-        .then((data) => {
-            if (data.success) {
-                document.getElementById('create-events-form').reset()
-                document.getElementById('messages').innerHTML = '<p>Event successfully added</p>'
-
-            } else {
-                document.getElementById('messages').innerHTML = '<p>Event not added</p>'
-            }
-        })
-
+        .then( eventInfo => eventInfo.json())
+        .then(eventInfo => displayHiringPartnerHandler(eventInfo.data))
 }
