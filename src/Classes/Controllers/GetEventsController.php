3caad77d4723ecb78797e4a87de0e054f381baf0
Rachmann <rachmann.joubert@gmail.com>
2019-10-02 15:52:08 +0100

    Changed message for default response and changed else statement to if

--- src/Classes/Controllers/GetEventsController.php
+++ src/Classes/Controllers/GetEventsController.php
@@ -33,7 +33,7 @@ class GetEventsController
     {
         $data = [
             'success' => false,
-            'message' => 'No events found!',
+            'message' => 'Something went wrong.',
             'data' => []
         ];
         $statusCode = 400;
@@ -47,14 +47,16 @@ class GetEventsController
         if (!empty($events)) {
             $data = [
                 'success' => true,
-                'message' => 'Query Successful',
+                'message' => 'Query Successful.',
                 'data' => $events
             ];
             $statusCode = 200;
-        } else {
+        }
+
+        if(empty($events)) {
             $data = [
                 'success' => true,
-                'message' => 'There is no event',
+                'message' => 'There are no events.',
                 'data' => []
             ];
             $statusCode = 200;
