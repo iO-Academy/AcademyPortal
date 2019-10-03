ce29ed8efcbc6f954189bb73d57172acd1e124d8
Rachmann <rachmann.joubert@gmail.com>
2019-10-03 11:07:42 +0100

    Added invoke

--- src/Classes/Controllers/AddContactController.php
+++ src/Classes/Controllers/AddContactController.php
@@ -3,6 +3,10 @@
 
 namespace Portal\Controllers;
 
+use Portal\Entities\HiringPartnerEntity;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
 class AddContactController
 {
 
@@ -16,4 +20,41 @@ class AddContactController
     {
         $this->hiringPartnerModel = $hiringPartnerModel;
     }
+
+    public function __invoke(Request $request, Response $response, array $args)
+    {
+        $newContact = $request->getParsedBody();
+        $data = [
+            'success' => false,
+            'message' => 'Error!',
+            'data' => []
+        ];
+        $statusCode = 400;
+
+        try {
+            $contact = $this->hiringPartnerModel->addNewContact(
+                $newContact['name'],
+                $newContact['email'],
+                $newContact['job_title'],
+                $newContact['phone'],
+                $newContact['hiring_partner_company_id'],
+                $newContact['is_primary_contact']
+            );
+            if (!empty($contact) && $contact instanceof HiringPartnerEntity) {
+                $result = $this->hiringPartnerModel->addNewContact($contact);
+            }
+        } catch (\Exception $exception) {
+            $data['message'] = $exception->getMessage();
+        }
+        if (isset($result) && $result) {
+            $data = [
+                'success' => true,
+                'message' => 'Hiring Partner Added to the db',
+                'data' => []
+            ];
+            $statusCode = 200;
+        }
+        return $response->withJson($data, $statusCode);
+    }
 }
+
