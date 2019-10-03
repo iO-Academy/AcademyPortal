d1fe3c4168af6e8a51108e190f3da1428b7c2680
Matthew Hammond <matthew.ian.hammond@gmail.com>
2019-10-03 10:41:06 +0100

    Created the html to add a contact to hiring partners

---
+++ src/Classes/ViewHelpers/SelectCompanyViewHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Portal\ViewHelpers;
+
+
+class selectCompanyViewHelper
+{
+    public static function selectCompanyDropdown($data)
+    {
+        $companySizeDropdown = '';
+        foreach ($data['companySize'] as $companySize) {
+            $companySizeDropdown .= '<option value=' . $companySize['id'] . '>' . $companySize['size'] . '</option>';
+        }
+        return $companySizeDropdown;
+    }
+}
