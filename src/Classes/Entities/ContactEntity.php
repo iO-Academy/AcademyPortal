ed3fb75edffea795bae31844b957eb19cfab8f06
Rachmann <rachmann.joubert@gmail.com>
2019-10-03 14:52:31 +0100

    Started reworking sanitiseData

--- src/Classes/Entities/ContactEntity.php
+++ src/Classes/Entities/ContactEntity.php
@@ -4,7 +4,7 @@
 namespace Portal\Entities;
 
 
-class ContactEntity
+class ContactEntity extends ValidationEntity
 {
     protected $contactName;
     protected $contactCompanyId;
@@ -34,5 +34,22 @@ class ContactEntity
         $this->sanitiseData();
     }
 
+    private function sanitiseData()
+    {
+        $this->contactName = self::sanitiseString($this->contactName);
+        $this->contactName = self::validateExistsAndLength($this->contactName, 255);
 
+        $this->category = (int)$this->category;
+        $this->category = self::validateCategoryExists($this->category, $this->eventCategories);
+        $this->location = self::sanitiseString($this->location);
+        $this->location = self::validateExistsAndLength($this->location, 255);
+        $this->date = $this->validateDate($this->date);
+        $this->startTime = $this->validateTime($this->startTime);
+        $this->endTime = $this->validateTime($this->endTime);
+        $this->validateStartEndTime($this->startTIme, $this->endTime);
+        if ($this->notes !== null) {
+            $this->notes = self::sanitiseString($this->notes);
+            $this->notes = self::validateLength($this->notes, 5000);
+        }
+    }
 }
