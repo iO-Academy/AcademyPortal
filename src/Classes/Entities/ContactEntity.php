fb6d617f1a6ab5a61b260044f162c86931eb541f
Rachmann <rachmann.joubert@gmail.com>
2019-10-03 14:37:58 +0100

    Created constructor

--- src/Classes/Entities/ContactEntity.php
+++ src/Classes/Entities/ContactEntity.php
@@ -6,11 +6,33 @@ namespace Portal\Entities;
 
 class ContactEntity
 {
-    protected $
-    protected
-    protected
-    protected
-    protected
-    protected
-    protected
+    protected $contactName;
+    protected $contactCompanyId;
+    protected $contactEmail;
+    protected $contactJobTitle;
+    protected $contactPhone;
+    protected $contactIsPrimary;
+
+    /**
+     * ContactEntity constructor.
+     */
+    public function __construct(
+        string $contactName = null,
+        int $contactCompanyId = null,
+        string $contactEmail = null,
+        string $contactJobTitle = null,
+        string $contactPhone = null,
+        int $contactIsPrimary = null
+    ) {
+        $this->contactName = ($this->contactName ?? $contactName);
+        $this->contactCompanyId = ($this->contactCompanyId ?? $contactCompanyId);
+        $this->contactEmail = ($this->contactEmail ?? $contactEmail);
+        $this->contactJobTitle = ($this->contactJobTitle ?? $contactJobTitle);
+        $this->contactPhone = ($this->contactPhone ?? $contactPhone);
+        $this->contactIsPrimary = ($this->contactIsPrimary ?? $contactIsPrimary);
+
+        $this->sanitiseData();
+    }
+
+
 }
