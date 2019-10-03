6c2819830d6d66f9b08b81e00595eedf13296be3
Rachmann <rachmann.joubert@gmail.com>
2019-10-03 10:14:27 +0100

    Created controller and constructor

---
+++ src/Classes/Controllers/AddContactController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Portal\Controllers;
+
+
+class AddContactController
+{
+
+    private $hiringPartnerModel;
+
+    /**
+     * AddContactController constructor.
+     * @param $hiringPartnerModel
+     */
+    public function __construct($hiringPartnerModel)
+    {
+        $this->hiringPartnerModel = $hiringPartnerModel;
+    }
+
+
+}
