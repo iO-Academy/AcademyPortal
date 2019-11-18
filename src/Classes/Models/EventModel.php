9fccb14437925a75612b44e9d0b204b1124eec98
cwoodwarduk <calum.james.woodward@gmail.com>
2019-11-18 15:10:16 +0000

    created first draft of linkHPToEvent function inside the Event Model. Ready for code review

--- src/Classes/Models/EventModel.php
+++ src/Classes/Models/EventModel.php
@@ -2,6 +2,8 @@
 
 namespace Portal\Models;
 
+use phpDocumentor\Reflection\Types\Boolean;
+use phpDocumentor\Reflection\Types\Integer;
 use Portal\Entities\EventEntity;
 
 class EventModel
@@ -76,6 +78,25 @@ class EventModel
         $query->bindParam(':startTime', $newEvent->getStartTime());
         $query->bindParam(':endTime', $newEvent->getEndTime());
         $query->bindParam(':notes', $newEvent->getNotes());
+        return $query->execute();
+    }
+
+
+    /**
+     * @param $hiringPartner the id of the hiring partner selected
+     *
+     * @param $event the id of the event selected
+     *
+     * @param $attendees the number of people attending from that hiring partner
+     *
+     * @return boolean True if operation succeeds
+     */
+    public function linkHPToEvent(Integer $hiringPartner, Integer $event, Integer $attendees) : bool
+    {
+        $query = $this->db->prepare('INSERT INTO `events_hiring_partner_link_table` (`hiring_partner_id`, `events_id`, `people_attending`) VALUES (:hiringPartner, :event, :attendees)');
+        $query->bindParam(':hiringPartner', $hiringPartner);
+        $query->bindParam(':event', $event);
+        $query->bindParam(':attendees', $attendees);
         return $query->execute();
     }
 }
