73572185fb12db1195709851b13d3d4c3aa1b23f
georginaw <watson.georgina@gmail.com>
2019-11-19 15:41:16 +0000

    updated the get events controller so that it incorporates the search events method from the EventModel and returns events matching search criteria to the view

--- src/Classes/Controllers/GetEventsController.php
+++ src/Classes/Controllers/GetEventsController.php
@@ -41,25 +41,7 @@ class GetEventsController
         $eventSearchInput = $request->getQueryParam('searchTerm');
         if (!empty($eventSearchInput)) {
             if (strlen($eventSearchInput) < 256) {
-            // run the search event function
-                $searchEvents = $this->eventModel->searchEvents($eventSearchInput);
-                if (!empty($eventSearchInput)) {
-                    $data = [
-                        'success' => true,
-                        'message' => 'Query Successful.',
-                        'data' => $eventSearchInput
-                    ];
-                    $statusCode = 200;
-                } else {
-                    $data = [
-                        'success' => true,
-                        'message' => 'There are no events.',
-                        'data' => []
-                    ];
-                    $statusCode = 200;
-                }
-                return $response->withJson($data, $statusCode);
-
+                $events = $this->eventModel->searchEvents($eventSearchInput);
             } else {
                 $data['message'] = 'Search term cannot be greater than 255 characters.';
                 return $response->withJson($data, $statusCode);
