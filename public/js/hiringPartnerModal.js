9ad31dd215a1722335345fa564f620a221bce9a8
James Taylor <taylor-j12@hotmail.com>
2019-11-19 16:57:42 +0000

    handled missing data in contact fields within modal

--- public/js/hiringPartnerModal.js
+++ public/js/hiringPartnerModal.js
@@ -1,11 +1,20 @@
 
+
 function validateField(data, field, noDataMessage = 'No information provided') {
-    console.log(data[field])
     if (data[field] === '' || data[field] === null || data[field] === undefined || data[field] === 0 || data[field] === false) {
         document.getElementById(field).innerHTML = noDataMessage
     } else {
         document.getElementById(field).innerHTML = data[field]
     }
+}
+
+function validateContactField(contactField, noDataMessage = 'No information provided') {
+    if (contactField === '' || contactField === null || contactField === undefined || contactField === 0 || contactField === false) {
+        return noDataMessage
+    } else {
+        return contactField
+    }
+
 }
 
 function addEventListenersForModal() {
@@ -25,26 +34,35 @@ function addEventListenersForModal() {
                             validateField(data[0], 'phone_number')
                             validateField(data[0], 'url_website')
 
-                let contactsHTML = '<h3>Company Contacts</h3>'
+                let contactsHTML = ''
 
                 delete data[0]
 
                 let yesNoSub = ['No', 'Yes']
 
                 data.forEach(function (contact) {
-                    contactsHTML += `
-                        <h4>Name</h4><p>${contact.name}</p>
-                        <h4>Email</h4><p>${contact.email}</p>
-                        <h4>Job title</h4><p>${contact.job_title}</p>
-                        <h4>Phone</h4><p>${contact.phone}</p>
-                        <h4>Primary Contact</h4><p>${yesNoSub[contact.is_primary_contact]}</p>`
 
+                    $validatedName = validateContactField(contact.name)
+                    $validatedEmail = validateContactField(contact.email)
+                    $validatedJobTitle = validateContactField(contact.job_title)
+                    $validatedPhone = validateContactField(contact.phone)
+                    $validatedIsPrimaryContact = validateContactField(yesNoSub[contact.is_primary_contact])
+
+                    contactsHTML += `
+                        <h4>Name</h4><p>${$validatedName}</p>
+                        <h4>Email</h4><p>${$validatedEmail}</p>
+                        <h4>Job title</h4><p>${$validatedJobTitle}</p>
+                        <h4>Phone</h4><p>${$validatedPhone}</p>
+                        <h4>Primary Contact</h4><p>${$validatedIsPrimaryContact}</p>`
                     contactsHTML += '<hr>'
                 })
 
                 let contactDetails = {}
 
                 contactDetails.contacts = contactsHTML
+
+                console.log('xxxxxxxx')
+                console.log(contactDetails)
 
                 validateField(contactDetails, 'contacts')
                     })
