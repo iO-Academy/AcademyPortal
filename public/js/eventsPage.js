9381215e336b9e3bcad5eedfee266fb416611c06
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-11-21 13:58:24 +0000

    moved clear search button to be on same row as search events input and button, changed colour of clear search button

--- public/js/eventsPage.js
+++ public/js/eventsPage.js
@@ -299,15 +299,16 @@ document.querySelector('#submit-search-event').addEventListener('click', functio
     e.preventDefault()
     if ((searchInput.length) && searchInput.length < 256) {
         getEvents(searchInput)
-        document.querySelector('#events-list').innerHTML = '<h2 id="event-search-results">Results</h2><a id="clear-search" class="btn btn-success" href="./displayEventsPage">Clear Search</a>'
-        document.querySelector('#clear-search').addEventListener('click', function(e) {
-            e.preventDefault()
-            if (!window.location.href.includes('#events-list')) {
-                window.location.href += '#events-list'
-            }
-            location.reload()
-        })
+        document.querySelector('#events-list').innerText = 'Results'
     } else {
         message.innerHTML = 'Event search: must be between 1 and 255 characters'
     }
+})
+
+document.querySelector('#clear-search').addEventListener('click', function(e) {
+    e.preventDefault()
+    if (!window.location.href.includes('#events-list')) {
+        window.location.href += '#events-list'
+    }
+    location.reload()
 })
