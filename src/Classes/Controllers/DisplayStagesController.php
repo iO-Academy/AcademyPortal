64867ac2f1b7a70f99917046014f5d9ab1d0c326
robings <r_gordon_stables@hotmail.com>
2020-03-31 14:53:30 +0100

    added DisplayStages Controller and Factory

---
+++ src/Classes/Controllers/DisplayStagesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Portal\Controllers;
+
+use \Slim\Http\Request as Request;
+use \Slim\Http\Response as Response;
+use Slim\Views\PhpRenderer;
+use Portal\Models\StageModel;
+
+class DisplayApplicantsController
+{
+    private $renderer;
+    private $stageModel;
+
+    /**
+     * DisplayStagesController constructor.
+     *
+     * @param PhpRenderer $renderer
+     *
+     * @param StageModel $stageModel
+     */
+    public function __construct(PhpRenderer $renderer, StageModel $stageModel)
+    {
+        $this->renderer = $renderer;
+        $this->stageModel = $stageModel;
+    }
+
+    public function __invoke(Request $request, Response $response, array $args)
+    {
+        return $this->renderer->render($response, 'displayStages.phtml', $args);
+    }
+
+}
---
+++ src/Classes/Factories/DisplayStagesControllerFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Portal\Factories;
+
+use Psr\Container\ContainerInterface;
+use Portal\Controllers\DisplayStagesController;
+
+class DisplayStagesControllerFactory
+    /**
+     * Instantiates DisplayStagesController with dependencies.
+     *
+     * @param ContainerInterface $container
+     *
+     * @return DisplayStagesController.
+     */
+    public function __invoke(ContainerInterface $container) : DisplayStagesController
+{
+    $renderer = $container->get('renderer');
+    $stageModel = $container->get('StageModel');
+    return new DisplayStagesController($renderer, $stageModel);
+}
