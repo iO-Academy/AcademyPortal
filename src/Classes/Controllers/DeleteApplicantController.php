ab83771cd12a8a2f59637fa26ae7a1f30c2e6d4d
paulms1 <stevenson.paulmichael@gmail.com>
2020-03-31 17:10:18 +0100

    fixed DeleteApplicantController Doc blocks to be adde

--- src/Classes/Controllers/DeleteApplicantController.php
+++ src/Classes/Controllers/DeleteApplicantController.php
@@ -17,13 +17,32 @@ class DeleteApplicantController
 
     public function __invoke(Request $request, Response $response, array $args)
     {
-        $requestData = $request->getParsedBody();
-        if (isset($requestData['id'])) {
-            if ($this->applicantModel->deleteApplicant($requestData['id'])) {
-                return $response->withStatus(200);
+        // if ($_SESSION['loggedIn'] === true) {
+            $data = ['success' => false, 'msg' => 'Applicant not found.'];
+            $response = $response->withStatus(500);
+            
+            $requestData = $request->getParsedBody();
+            
+            if (isset($requestData['id'])) {
+                $validatedRequestData = filter_var($requestData['id'], FILTER_VALIDATE_INT);
+                
+                if ($validatedRequestData) {
+                    $applicantData = $this->applicantModel->getApplicantById($validatedRequestData);
+                    if ($applicantData) {
+                        if ($this->applicantModel->deleteApplicant($validatedRequestData)) {
+                            $data = ['success' => true, 'msg' => 'Applicant has been deleted successfully.'];
+                            $response = $response->withStatus(200);
+                        } 
+                    }
+                } else {
+                    $data = ['msg' => 'Invalid id provided.'];
+                }
             } else {
-                return $response->withStatus(500);
+                $data = ['msg' => 'No id provided.'];
             }
-        }
+
+            return $response->withJson($data);
+
+        // }
     }
 }
