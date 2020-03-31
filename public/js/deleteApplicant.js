b7ec42d193cfe882e884388c628c0a69098a3336
ajhswan <andrewswan.dev@gmail.com>
2020-03-31 13:03:35 +0100

    Add eventlistener to delete button, added temp divs for success or failure massges

--- public/js/deleteApplicant.js
+++ public/js/deleteApplicant.js
@@ -3,7 +3,24 @@ let deleteButton = document.getElementById('deleteBtn')
 
     deleteButton.addEventListener('click', function (e) {
         let id = e.target.dataset.id
+            console.log(id)
 
+        // for testing move into fetch
+        let elem = document.getElementById('deleteResponse')
+        let foo = false
+        if (foo == false) {
+            let message = 'response=Applicant deleted successfully!'
+            document.cookie = message
+            window.location.href = "http://localhost:8080/displayApplicants";
+            let cookie = document.cookie
+            console.log(cookie)
+            elem.textContent = getCookie('response')
+
+
+
+        } else {
+            elem.textContent = 'Error - there was a problem, please try again or contact your administrator'
+        }
         fetch('/api/deleteApplicant', {
             method: 'DELETE',
             body: JSON.stringify({"id": id}),
@@ -11,14 +28,21 @@ let deleteButton = document.getElementById('deleteBtn')
                 "Content-Type": "application/json"
             } .then(function(response){
             console.log(response.status)
-                if (response.status) === 200 {
-                    let elem = document
+                let elem = document.getElementById('deleteResponse')
+                if (response.status === 200) {
+
+                    //set cooking message to success
+                    window.location.reload(true)
+                    elem.textContent = 'Applicant deleted successfully!'
                     } else {
-                    //display
+                    //error cookie set message to error.
+                    elem.textContent = 'Error - there was a problem deleting applicant, please try again or contact your administrator'
                 }
 
         })
 
         });
-
-    })
+ // add eventListen on page load
+window.addEventListener('reload', function(e) {
+        console.log('hello')
+})
