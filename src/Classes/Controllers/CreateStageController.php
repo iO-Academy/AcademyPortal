3688727f65715dbf1e0ea42cfa384c1c430b1e63
markandmarkup <markeyles88@gmail.com>
2020-04-01 13:59:07 +0100

    Controller complete, and createStage method added to stage model.

--- src/Classes/Controllers/CreateStageController.php
+++ src/Classes/Controllers/CreateStageController.php
@@ -21,6 +21,14 @@ class CreateStageController
         $this->stageModel = $stageModel;
     }
 
+    /** On invoke, check request input for Title value, then create new StageEntity with incremented
+     * order number to send to DB via StageModel
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     * @return Response - with json and status 200/500 for success or failure.
+     */
     public function __invoke(Request $request, Response $response, array $args)
     {
         $requestData = $request->getParsedBody();
@@ -31,38 +39,24 @@ class CreateStageController
         ];
         $statusCode = 500;
 
+        if ($_SESSION['loggedIn'] === true) {
+            if (isset($requestData['title']) && strlen($requestData['title'] > 0)) {
+                $highestOrder = $this->stageModel->getHighestOrderNo();
+                $newStage = new StageEntity($requestData['title'], ++$highestOrder);
+                if ($this->stageModel->createStage($newStage)) {
+                    $data = [
+                        'success' => true,
+                        'message' => 'Stage added successfully',
+                    ];
+                    $statusCode = 200;
+                } else {
+                    $data = [
+                        'message' => 'Error adding to database',
+                    ];
+                }
 
-        if (isset($requestData['title'])) {
-            $highestOrder = $this->stageModel->getHighestOrderNo();
-
+                return $response->withJson($data, $statusCode);
+            }
         }
-
-//        $newStage = new StageEntity($title, $order);
-//
-//        try {
-//            $stage = $this->stageEn->createNewHiringPartner(
-//                $newHiringPartner['name'],
-//                $newHiringPartner['companySize'],
-//                $newHiringPartner['techStack'],
-//                $newHiringPartner['postcode'],
-//                $newHiringPartner['phoneNumber'],
-//                $newHiringPartner['companyUrl']
-//            );
-//            if (!empty($hiringPartner) && $hiringPartner instanceof HiringPartnerEntity) {
-//                $result = $this->hiringPartnerModel->addHiringPartner($hiringPartner);
-//            }
-//        } catch (\Exception $exception) {
-//            $data['message'] = $exception->getMessage();
-//        }
-//
-//        if (isset($result) && $result) {
-//            $data = [
-//                'success' => true,
-//                'message' => 'Hiring Partner Added to the db',
-//                'data' => []
-//            ];
-//            $statusCode = 200;
-//        }
-//        return $response->withJson($data, $statusCode);
     }
 }
--- src/Classes/Models/StageModel.php
+++ src/Classes/Models/StageModel.php
@@ -2,20 +2,42 @@
 
 namespace Portal\Models;
 
+use \Portal\Entities\StageEntity;
+
 class StageModel
 {
     private $db;
 
+    /** Constructor assigns db PDO to this object
+     *
+     * StageModel constructor.
+     * @param \PDO $db
+     */
     public function __construct(\PDO $db)
     {
         $this->db = $db;
     }
 
+    /**
+     * @return int
+     */
     public function getHighestOrderNo() : int
     {
-        $query = $this->db->prepare('SELECT MAX(`order`) FROM `stages` WHERE `deleted` = 0;');
+        $query = $this->db->prepare("SELECT MAX(`order`) FROM `stages` WHERE `deleted` = 0;");
         $query->execute();
         $result = $query->fetch();
-        return $result['MAX(`order`)'];
+        return ($result['MAX(`order`)'] ?? 0);
+    }
+
+    /**
+     * @param StageEntity $stageEntity
+     * @return bool
+     */
+    public function createStage(StageEntity $stageEntity)
+    {
+        $query = $this->db->prepare("INSERT INTO `stages` (`title`, `order`) VALUES (:title, :order);");
+        $query->bindParam(':title', $stageEntity->getTitle());
+        $query->bindParam(':order', $stageEntity->getOrder());
+        return $query->execute();
     }
 }
