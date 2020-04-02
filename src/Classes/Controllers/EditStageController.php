fd8e9c17974220f2c1072beaf4d1f892c4b946b2
Alex Stone Lunde <alexlunde@gmail.com>
2020-04-02 15:01:51 +0100

    validation in EditStageController

--- src/Classes/Controllers/EditStageController.php
+++ src/Classes/Controllers/EditStageController.php
@@ -3,11 +3,13 @@
 namespace Portal\Controllers;
 
 use Portal\Entities\StageEntity;
+use Portal\Entities\ValidationEntity;
 use \Slim\Http\Request as Request;
 use \Slim\Http\Response as Response;
 use Portal\Models\StageModel;
+use mysql_xdevapi\Exception;
 
-class EditStageController
+class EditStageController extends ValidationEntity
 {
     private $stageModel;
 
@@ -44,7 +46,18 @@ class EditStageController
 
                 foreach ($requestDataPackage['data'] as $stageObject) {
 
-                    if (isset($stageObject['id'])) {
+
+                    if (isset($stageObject['id']) && isset($stageObject['title']) && isset($stageObject['order'])) {
+
+                        $stageObject['id'] = (int) $stageObject['id'];
+                        $stageObject['title'] = self::sanitiseString($stageObject['title']);
+                        try {
+                            $stageObject['title'] = self::validateLength($stageObject['title'], 255);
+                        } catch (\Exception $exception) {
+                            $stageObject['title'] = substr($stageObject['title'], 0, 254);
+                        }
+
+                        $stageObject['order'] = (int) $stageObject['order'];
 
                         $this->stageModel->updateStage($stageObject['id'], $stageObject['title'], $stageObject['order']);
 
@@ -72,4 +85,22 @@ class EditStageController
             }
         }
 //    }
+
+    /**
+     * Will sanitise all the fields for a stage.
+     */
+    private function sanitiseData()
+    {
+        $this->id = (int) $this->id;
+        $this->title = self::sanitiseString($this->title);
+
+        try {
+            $this->title = self::validateLength($this->title, 255);
+        } catch (\Exception $exception) {
+            $this->title = substr($this->title, 0, 254);
+        }
+
+        $this->order = (int) $this->order;
+        $this->deleted = (int) $this->deleted;
+    }
 }
