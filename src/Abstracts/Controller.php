586ee6cc873f907adc97115d395becf8a23f9f2d
Ashley Coles <ashley@acoles.co.uk>
2020-05-11 10:11:04 +0100

    Removing empty line

--- src/Abstracts/Controller.php
+++ src/Abstracts/Controller.php
@@ -28,5 +28,4 @@ abstract class Controller
         $response->getBody()->write($json);
         return $response->withHeader('Content-type', 'application/json')->withStatus($statusCode);
     }
-
 }
