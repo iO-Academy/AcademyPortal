1721195bf097032aa33a84752309078818c360d6
Mike Oram Front End Developer <mike.oram@mayden.co.uk>
2020-05-19 14:56:08 +0100

    fix front end to work with sensible data structure

--- public/js/hiringPartnerModal.js
+++ public/js/hiringPartnerModal.js
@@ -26,41 +26,39 @@ function addEventListenersForModal() {
                     function(response) {
                         return response.json()
                     }).then(function(data) {
-                            validateField(data[0], 'name')
-                            validateField(data[0], 'size')
-                            validateField(data[0], 'tech_stack')
-                            validateField(data[0], 'postcode')
-                            validateField(data[0], 'phone_number')
-                            validateField(data[0], 'url_website')
+                        validateField(data.data.company, 'name')
+                        validateField(data.data.company, 'size')
+                        validateField(data.data.company, 'tech_stack')
+                        validateField(data.data.company, 'postcode')
+                        validateField(data.data.company, 'phone_number')
+                        validateField(data.data.company, 'url_website')
 
-                let contactsHTML = ''
+                        let contactsHTML = ''
 
-                delete data[0]
+                        let yesNoSub = ['No', 'Yes']
 
-                let yesNoSub = ['No', 'Yes']
+                        data.data.contacts.forEach(function (contact) {
 
-                data.forEach(function (contact) {
+                            let validatedName = validateContactField(contact.name)
+                            let validatedEmail = validateContactField(contact.email)
+                            let validatedJobTitle = validateContactField(contact.job_title)
+                            let validatedPhone = validateContactField(contact.phone)
+                            let validatedIsPrimaryContact = validateContactField(yesNoSub[contact.is_primary_contact])
 
-                    let validatedName = validateContactField(contact.name)
-                    let validatedEmail = validateContactField(contact.email)
-                    let validatedJobTitle = validateContactField(contact.job_title)
-                    let validatedPhone = validateContactField(contact.phone)
-                    let validatedIsPrimaryContact = validateContactField(yesNoSub[contact.is_primary_contact])
+                            contactsHTML += `
+                                <h4>Name</h4><p>${validatedName}</p>
+                                <h4>Email</h4><p>${validatedEmail}</p>
+                                <h4>Job title</h4><p>${validatedJobTitle}</p>
+                                <h4>Phone</h4><p>${validatedPhone}</p>
+                                <h4>Primary Contact</h4><p>${validatedIsPrimaryContact}</p>`
+                            contactsHTML += '<hr>'
+                        })
 
-                    contactsHTML += `
-                        <h4>Name</h4><p>${validatedName}</p>
-                        <h4>Email</h4><p>${validatedEmail}</p>
-                        <h4>Job title</h4><p>${validatedJobTitle}</p>
-                        <h4>Phone</h4><p>${validatedPhone}</p>
-                        <h4>Primary Contact</h4><p>${validatedIsPrimaryContact}</p>`
-                    contactsHTML += '<hr>'
-                    })
+                        let contactDetails = {}
 
-                    let contactDetails = {}
+                        contactDetails.contacts = contactsHTML
 
-                    contactDetails.contacts = contactsHTML
-
-                    validateField(contactDetails, 'contacts')
+                        validateField(contactDetails, 'contacts')
                     })
                 .catch(function() {
                     document.querySelector('#contacts').innerText = 'We were unable to retrieve the' +
