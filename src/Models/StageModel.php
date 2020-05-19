3233e74ad28bf0c1025f19d71a1990480a6dee23
“Milo <milogirardet@gmail.com>
2020-05-19 11:58:24 +0100

    reverted getstagebyid query

--- src/Models/StageModel.php
+++ src/Models/StageModel.php
@@ -113,13 +113,7 @@ class StageModel
      */
     public function getStageById(int $id) : StageEntity
     {
-        $query = $this->db->prepare(
-            'SELECT `stages`.`id`, `stages`.`title`, `stages`.`order`, `stages`.`deleted`, `options`.`option` 
-                FROM `stages` 
-                    INNER JOIN `options` 
-                        ON `stages`.`id` = `options`.`stageId` 
-                            WHERE `stages`.`id`=:id;
-        ');
+        $query = $this->db->prepare('SELECT `id`, `title`, `order`, `deleted` FROM `stages` WHERE `id`=:id');
 
         $query->setFetchMode(\PDO::FETCH_CLASS, 'Portal\Entities\StageEntity');
         $query->bindParam(':id', $id);
