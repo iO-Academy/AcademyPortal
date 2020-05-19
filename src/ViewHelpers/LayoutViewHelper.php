48bba7f83819b923cef3d7c363cd4964b9c93699
Mike Oram Front End Developer <mike.oram@mayden.co.uk>
2020-05-19 17:26:30 +0100

    tidied up front end of admin panel and applicants pages

---
+++ src/ViewHelpers/LayoutViewHelper.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Portal\ViewHelpers;
+
+
+class LayoutViewHelper
+{
+    public static function outputTitle(string $title): string
+    {
+        return '<title>AcademyPortal | ' . $title . '</title>';
+    }
+
+}
