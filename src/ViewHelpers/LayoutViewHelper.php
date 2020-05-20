bd762a9bf9ce1cf890ceed48e4a5c3748d107f9a
Mike Oram Front End Developer <mike.oram@mayden.co.uk>
2020-05-20 17:20:07 +0100

    fixing PSR issues

--- src/ViewHelpers/LayoutViewHelper.php
+++ src/ViewHelpers/LayoutViewHelper.php
@@ -3,12 +3,10 @@
 
 namespace Portal\ViewHelpers;
 
-
 class LayoutViewHelper
 {
     public static function outputTitle(string $title): string
     {
         return '<title>AcademyPortal | ' . $title . '</title>';
     }
-
 }
