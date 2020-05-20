7213e9898335334bd9adfb55f3a687b9e34f1b56
Alex Jennings <jenningsalexyt@gmail.com>
2020-05-20 11:35:14 +0100

    Corrected indentation on DisplayApplicantViewHelper.

--- src/ViewHelpers/DisplayApplicantViewHelper.php
+++ src/ViewHelpers/DisplayApplicantViewHelper.php
@@ -19,26 +19,26 @@ class DisplayApplicantViewHelper
         foreach ($applicants as $applicant) {
             if ($applicant instanceof ApplicantEntity) {
                 $result .= '<tr>
-                        <td><a data-id ="'. $applicant->getId().'" type="button"  class="myBtn">'
-                            . $applicant->getName() .'</a></td>
-                            <td>'. $applicant->getEmail() .'</td>
-                            <td>'. $applicant->getDateOfApplication() .'</td>
-                            <td>'. $applicant->getCohortDate().'</td>
-                            <td>
-                            <button id=\'editBtn\'
-                              type="button"
-                              class="btn btn-primary edit"
-                              data-id="' . $applicant->getId() . '">
-                              Edit
-                              </button>
-                             <button id=\'deleteBtn\'
-                              type="button"
-                              class="btn btn-danger delete"
-                              data-id="' . $applicant->getId() . '">
-                              Delete
-                              </button>
-                            </td>
-                        </tr>';
+                                <td><a data-id ="'. $applicant->getId().'" type="button"  class="myBtn">'
+                                     . $applicant->getName() .'</a></td>
+                                <td>'. $applicant->getEmail() .'</td>
+                                <td>'. $applicant->getDateOfApplication() .'</td>
+                                <td>'. $applicant->getCohortDate().'</td>
+                                <td>
+                                    <button id=\'editBtn\'
+                                            type="button"
+                                            class="btn btn-primary edit"
+                                            data-id="' . $applicant->getId() . '">
+                                            Edit
+                                    </button>
+                                    <button id=\'deleteBtn\'
+                                            type="button"
+                                            class="btn btn-danger delete"
+                                            data-id="' . $applicant->getId() . '">
+                                            Delete
+                                    </button>
+                                </td>
+                            </tr>';
             }
         }
         return ($result);
