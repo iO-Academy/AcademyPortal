9a9639b081130192b4517aad447a7f729bfc705d
turnerror <christianturner@hotmail.co.uk>
2020-05-20 15:37:01 +0100

    hearAbout.hearAbout is no longer

--- public/js/addApplicant.js
+++ public/js/addApplicant.js
@@ -7,44 +7,44 @@ let getFormOptions = () => {
             'Content-Type': 'application/json',
         },
     }).then(response => response.json())
-}
+};
 
 let handleFormOptions = async () => {
-    let response = await getFormOptions()
+    let response = await getFormOptions();
     return response.data
-}
+};
 
 let outputCohorts = async () => {
     const data = await handleFormOptions();
     const element = document.getElementById('cohorts');
     let cohortOptions = '';
 
-    data.cohorts.forEach(cohort => {
-        if (element.dataset.selected === cohort.id) {
-            cohortOptions += `<option selected value="${cohort.id}">${cohort.date}</option>`
-        } else {
-            cohortOptions += `<option value="${cohort.id}">${cohort.date}</option>`
+    data.cohorts.forEach(item => {
+        cohortOptions += "<option ";
+        if (element.dataset.selected === item.id) {
+            cohortOptions += "selected ";
         }
+        cohortOptions += `value="${item.id}">${item.date}</option>`;
     });
 
-    element.innerHTML = cohortOptions
+    element.innerHTML = cohortOptions;
 };
 
 let outputHearAbout = async () => {
     let data = await handleFormOptions();
-    let element= document.getElementById('hear-about');
+    let element = document.getElementById('hear-about');
     let hearAboutOptions = '';
 
-    data.hearAbout.forEach(hearAbout => {
-        if (element.dataset.selected === hearAbout.id) {
-            hearAboutOptions += `<option selected value="${hearAbout.id}">${hearAbout.hearAbout}</option>`
-        } else {
-            hearAboutOptions += `<option value="${hearAbout.id}">${hearAbout.hearAbout}</option>`
+    data.hearAbout.forEach(item => {
+        hearAboutOptions += "<option ";
+        if (element.dataset.selected === item.id) {
+            hearAboutOptions += "selected ";
         }
+        hearAboutOptions += `value="${item.id}">${item.hearAbout}</option>`;
     });
 
     element.innerHTML = hearAboutOptions;
 };
 
-outputCohorts()
-outputHearAbout()
+outputCohorts();
+outputHearAbout();
