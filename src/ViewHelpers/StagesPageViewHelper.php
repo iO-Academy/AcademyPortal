a918a779ddb16c8292b6a34f6647c29c3457f6b9
Richard Herbert <richard.herbert1980@gmail.com>
2020-05-20 14:27:42 +0100

    fix tests

--- src/ViewHelpers/StagesPageViewHelper.php
+++ src/ViewHelpers/StagesPageViewHelper.php
@@ -23,33 +23,40 @@ class StagesPageViewHelper
                 $result .= '<td>';
                 $result .= '<p>'. $stage->getStageTitle().'</p>';
                 $result .= '<form data-id="'. $stage->getStageId().'" class="stagesTableForm">';
-                $result .= '<input type="text" class="stageEditTitle col-xs-10" value="'
-                    . $stage->getStageTitle().'"/>';
+                $result .= '<input type="text" class="stageEditTitle ';
+                $result .= 'col-xs-10" value="'. $stage->getStageTitle().'"/>';
                 $result .= '<input type="submit" class="stageEditSubmit btn-success" value="Submit">';
                 $result .= '</form>';
                 $result .= '<div class="optionsContainer hide" data-stageId="'. $stage->getStageId().'">';
                 if(empty($stage->getOptions())){
                     $result .= '<div class="optionContainer multiOptionsContainer">';
                     $result .= '<form data-id="" class="optionAddForm">';
-                    $result .= '<input type="text" class="optionAddTitle firstOption col-xs-10" placeholder="Type the name of your new option"/>';
+                    $result .= '<input type="text" class="optionAddTitle ';
+                    $result .= 'firstOption col-xs-10" placeholder="Type the name of your new option"/>';
                 } else {
                     foreach($stage->getOptions() as $option){
                         $result .= '<div class="optionContainer">';
-                        $result .= '<p class="optionTitle" data-optionId="'.$option->getOptionId().'">'. $option->getOptionTitle();
-                        $result .= '<a class="text-danger optionDelete" data-optionId="'.$option->getOptionId().'">Delete</a>';
+                        $result .= '<p class="optionTitle" data-optionId="';
+                        $result .= $option->getOptionId().'">'. $option->getOptionTitle();
+                        $result .= '<a class="text-danger optionDelete" data-optionId="';
+                        $result .= $option->getOptionId().'">Delete</a>';
                         $result .= '<a class="optionEdit" data-optionId="'.$option->getOptionId().'">Edit</a>';
                         $result .= '</p>';
                         $result .= '<form class="optionTableForm hide" data-optionId="'.$option->getOptionId().'">';
-                        $result .= '<input type="text" class="optionEditTitle col-xs-10" value="'. $option->getOptionTitle().'"/>';
-                        $result .= '<input type="submit" class="optionEditSubmit btn-success" value="Submit" data-optionid="'.$option->getOptionId().'">';
+                        $result .= '<input type="text" class="optionEditTitle col-xs-10" value="';
+                        $result .= $option->getOptionTitle().'"/>';
+                        $result .= '<input type="submit" class="optionEditSubmit btn-success" ';
+                        $result .= 'value="Submit" data-optionid="'.$option->getOptionId().'">';
                         $result .= '</form>';
                         $result .= '</div>';
                     }
                     $result .= '<div class="optionContainer">';
                     $result .= '<form data-id="" class="optionAddForm ">';
                 }
-                $result .= '<input type="text" class="optionAddTitle col-xs-10" placeholder="Type the name of your new option"/>';
-                $result .= '<input type="submit" class="optionAddSubmit btn-success" data-stageid="'.$stage->getStageId().'" value="Submit">';
+                $result .= '<input type="text" class="optionAddTitle col-xs-10" ';
+                $result .= 'placeholder="Type the name of your new option"/>';
+                $result .= '<input type="submit" class="optionAddSubmit btn-success" data-stageid="';
+                $result .= $stage->getStageId().'" value="Submit">';
                 $result .= '</form>';
                 $result .= '</div>';
                 $result .= '</div>';
@@ -58,7 +65,8 @@ class StagesPageViewHelper
                 $result .= '<td class="col-xs-2 text-center"><a class="toggleEditForm">Edit</a></td>';
                 $result .= '<td class="col-xs-2 text-center"><a data-id="'. $stage->getStageId()
                     .'" class="text-danger delete">Delete</a></td>';
-                $result .= '<td class="col-xs-2 text-center"><a class="toggleEditOptions" data-stageId="'.$stage->getStageId().'">Options</a></td>';
+                $result .= '<td class="col-xs-2 text-center"><a class="toggleEditOptions" data-stageId="';
+                $result .= $stage->getStageId().'">Options</a></td>';
                 $result .= '</tr>';
             }
         }
--- tests/ViewHelpers/StagesPageViewHelperTest.php
+++ tests/ViewHelpers/StagesPageViewHelperTest.php
@@ -5,6 +5,7 @@ namespace Tests\ViewHelpers;
 use PHPUnit\Framework\TestCase;
 use Portal\ViewHelpers\StagesPageViewHelper;
 use Portal\Entities\StageEntity;
+use Portal\Entities\OptionsEntity;
 
 class StagesPageViewHelperTest extends TestCase
 {
@@ -15,22 +16,49 @@ class StagesPageViewHelperTest extends TestCase
     public function testSuccessDisplayStages()
     {
         $expected = '';
-        $expected .= '<tr class="list-group-item" data-id="1">';
-        $expected .=      '<td>';
-        $expected .=          '<p>Stage 1 test</p>';
-        $expected .=          '<form data-id="1" class="stagesTableForm">';
-        $expected .=              '<input type="text" class="stageEditTitle col-xs-10" value="Stage 1 test"/>';
-        $expected .=              '<input type="submit" class="stageEditSubmit btn-success" value="Submit">';
-        $expected .=          '</form>';
-        $expected .=      '</td>';
-        $expected .=      '<td class="col-xs-2 text-center"><a class="toggleEditForm">Edit</a></td>';
-        $expected .=      '<td class="col-xs-2 text-center"><a data-id="1" class="text-danger delete">Delete</a></td>';
+        $expected .= '<tr class="" data-id="1">';
+        $expected .= '<td>';
+        $expected .= '<p>Stage 1 test</p>';
+        $expected .= '<form data-id="1" class="stagesTableForm">';
+        $expected .= '<input type="text" class="stageEditTitle col-xs-10" value="Stage 1 test"/>';
+        $expected .= '<input type="submit" class="stageEditSubmit btn-success" value="Submit">';
+        $expected .= '</form>';
+        $expected .= '<div class="optionsContainer hide" data-stageId="1">';
+        $expected .= '<div class="optionContainer">';
+        $expected .= '<p class="optionTitle" data-optionId="1">aTitle';
+        $expected .= '<a class="text-danger optionDelete" data-optionId="1">Delete</a>';
+        $expected .= '<a class="optionEdit" data-optionId="1">Edit</a>';
+        $expected .= '</p>';
+        $expected .= '<form class="optionTableForm hide" data-optionId="1">';
+        $expected .= '<input type="text" class="optionEditTitle col-xs-10" value="aTitle"/>';
+        $expected .= '<input type="submit" class="optionEditSubmit btn-success" value="Submit" data-optionid="1">';
+        $expected .= '</form>';
+        $expected .= '</div>';
+        $expected .= '<div class="optionContainer">';
+        $expected .= '<form data-id="" class="optionAddForm ">';
+        $expected .= '<input type="text" class="optionAddTitle col-xs-10" placeholder="Type the name of your new ';
+        $expected .= 'option"/>';
+        $expected .= '<input type="submit" class="optionAddSubmit btn-success" data-stageid="1" value="Submit">';
+        $expected .= '</form>';
+        $expected .= '</div>';
+        $expected .= '</div>';
+        $expected .= '</div>';
+        $expected .= '</td>';
+        $expected .= '<td class="col-xs-2 text-center"><a class="toggleEditForm">Edit</a></td>';
+        $expected .= '<td class="col-xs-2 text-center"><a data-id="1" class="text-danger delete">Delete</a></td>';
+        $expected .= '<td class="col-xs-2 text-center"><a class="toggleEditOptions" data-stageId="1">Options</a></td>';
         $expected .= '</tr>';
-        $entityMock = $this->createMock(StageEntity::class);
-        $entityMock->method('getStageId')->willReturn('1');
-        $entityMock->method('getStageTitle')->willReturn('Stage 1 test');
 
-        $input =  [$entityMock];
+        $objectEntityMock = $this->createMock(OptionsEntity::class);
+        $objectEntityMock->method('getOptionId')->willReturn('1');
+        $objectEntityMock->method('getOptionTitle')->willReturn('aTitle');
+
+        $stageEntityMock = $this->createMock(StageEntity::class);
+        $stageEntityMock->method('getStageId')->willReturn('1');
+        $stageEntityMock->method('getStageTitle')->willReturn('Stage 1 test');
+        $stageEntityMock->method('getOptions')->willReturn([$objectEntityMock]);
+
+        $input =  [$stageEntityMock];
         $case = StagesPageViewHelper::displayStages($input);
 
         $this->assertEquals($expected, $case);
