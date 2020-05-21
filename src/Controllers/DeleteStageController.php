eb37c3af36903bffce5411391e15fca4c26091f1
HiredMussel <alexmusselwhite2@gmail.com>
2020-05-21 11:54:42 +0100

    add login checks back in

--- src/Controllers/DeleteStageController.php
+++ src/Controllers/DeleteStageController.php
@@ -32,42 +32,42 @@ class DeleteStageController extends Controller
      */
     public function __invoke(Request $request, Response $response, array $args)
     {
-        // if ($_SESSION['loggedIn'] === true) {
-        $data = [
+        if ($_SESSION['loggedIn'] === true) {
+            $data = [
                 'success' => false,
                 'message' => 'Stage not found.',
                 'data' => []
             ];
-        $statusCode = 500;
+            $statusCode = 500;
 
-        if ($this->stageModel->stagesCount() <= 1) {
-            $data['message'] = 'Cannot delete last stage.';
-            $statusCode = 400;
-        } else {
-            $requestData = $request->getParsedBody();
-            if (isset($requestData['id']) && filter_var($requestData['id'], FILTER_VALIDATE_INT)) {
-                $stageData = $this->stageModel->getStageById($requestData['id']);
-                if ($stageData) {
-                    if (empty($stageData->getOptions())) {
-                        if ($this->stageModel->deleteStage($requestData['id'])) {
-                            $data = [
+            if ($this->stageModel->stagesCount() <= 1) {
+                $data['message'] = 'Cannot delete last stage.';
+                $statusCode = 400;
+            } else {
+                $requestData = $request->getParsedBody();
+                if (isset($requestData['id']) && filter_var($requestData['id'], FILTER_VALIDATE_INT)) {
+                    $stageData = $this->stageModel->getStageById($requestData['id']);
+                    if ($stageData) {
+                        if (empty($stageData->getOptions())) {
+                            if ($this->stageModel->deleteStage($requestData['id'])) {
+                                $data = [
                                     'success' => true,
                                     'message' => 'Stage has been deleted successfully.',
                                     'data' => []
                                 ];
-                            $statusCode = 200;
+                                $statusCode = 200;
+                            }
+                        } else {
+                            $data['message'] = 'Cannot delete stage with options still in it. Delete options first.';
+                            $statusCode = 400;
                         }
-                    } else {
-                        $data['message'] = 'Cannot delete stage with options still in it. Delete options first.';
-                        $statusCode = 400;
                     }
+                } else {
+                    $statusCode = 400;
+                    $data['message'] = 'Invalid id provided.';
                 }
-            } else {
-                $statusCode = 400;
-                $data['message'] = 'Invalid id provided.';
             }
+            return $this->respondWithJson($response, $data, $statusCode);
         }
-        return $this->respondWithJson($response, $data, $statusCode);
-        // }
     }
 }
