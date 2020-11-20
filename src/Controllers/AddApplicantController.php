f16cbea20b09421e09310cd4ce15ebda1df937f7
Mike Oram <mike.oram@mayden.co.uk>
2020-11-20 16:34:32 +0000

    fixed it

--- src/Controllers/AddApplicantController.php
+++ src/Controllers/AddApplicantController.php
@@ -21,4 +21,22 @@ class AddApplicantController extends Controller
         $this->renderer = $renderer;
     }
 
+    /**
+     * Checks if the users are logged in or not.
+     *
+     * @param Request $request HTTP request
+     * @param Response $response HTTP response
+     * @param $args array
+     *
+     * @return Response will return the url output.
+     */
+    public function __invoke(Request $request, Response $response, array $args)
+    {
+        if ($_SESSION['loggedIn'] === true) {
+            return $this->renderer->render($response, 'newApplicantForm.phtml');
+        }
+
+        $_SESSION['loggedIn'] = false;
+        return $response->withHeader('Location', './');
+    }
 }
