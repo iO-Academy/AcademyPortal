6c499b003aeb7d832f75509c7bbe2578e19ef210
Jess Tapley <jjtapley@aol.com>
2020-11-24 16:31:20 +0000

    Unit tests for GetStagesController/Factory

---
+++ tests/Factories/GetStagesControllerFactoryTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Factories;
+
+use PHPUnit\Framework\TestCase;
+use Portal\Models\StageModel;
+use Psr\Container\ContainerInterface;
+use Portal\Controllers\API\GetStagesController;
+use Portal\Factories\GetStagesControllerFactory;
+
+
+
+class GetStagesControllerFactoryTest extends TestCase
+{
+    public function testInvoke()
+    {
+        $container = $this->createMock(ContainerInterface::class);
+        $stage = $this->createMock(StageModel::class);
+        $container->method('get')
+            ->willReturn($stage);
+
+        $factory = new GetStagesControllerFactory;
+        $case = $factory($container);
+        $expected = GetStagesController::class;
+        $this->assertInstanceOf($expected, $case);
+    }
+}
