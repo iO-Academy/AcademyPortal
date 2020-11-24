28a3303ab31e700c7b97ef4690fea58f409a997b
El Stevens <eleanorkwstevens@gmail.com>
2020-11-24 16:35:44 +0000

    created view helper, inserted dropdown code into there and linked to phtml file

---
+++ src/ViewHelpers/ApplicantFormViewHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Portal\ViewHelpers;
+
+
+class ApplicantFormViewHelper
+{
+    public static function stagesDropdown($stages, $stageOptions)
+    {
+        $string = '';
+        foreach ($stages as $stage) {
+            $stageHasOptions = false;
+            $optionsForThisStage = [];
+            foreach ($stageOptions as $stageOption) {
+                if ($stageOption['stageId'] == $stage['id']) {
+                    $stageHasOptions = true;
+                    $optionsForThisStage[] = $stageOption;
+                }
+            }
+            if($stageHasOptions) {
+                $string .= '<optgroup label="' . $stage['title'] . '">';
+
+                foreach ($optionsForThisStage as $option) {
+                    $string .= '<option value="' . $option['id'] . '">' . $option['option'] . '</option>';
+                }
+
+                $string .= '</optgroup>';
+            } else {
+                $string .= '<option class="stageOptionsDropdown" value="' . $stage['id'] . '">' . $stage['title'] . '</option>';
+            }
+        }
+        return $string;
+    }
+}
