254fd15f734e9e1803857133ccb3576063c1a17e
Davin <davin.hayden@outlook.com>
2020-11-25 09:45:44 +0000

    sorted out modal to display options for the currently hard-coded stage id

--- public/js/nextStageModal.js
+++ public/js/nextStageModal.js
@@ -5,38 +5,22 @@ $(document).ready(function(){
         fetch(url)
             .then(
                 function(response) {
-                    console.log('anything');
                     if (response.status !== 200) {
-                        console.log('line 10');
                         document.querySelector('#modal-main').innerHTML = ''
                         document.querySelector('#modal-main').innerHTML += '<div class="alert alert-danger" role="alert">Looks like there was a problem. Status Code: ' +
                             response.status + '</div>'
-                        console.log('bad shit');
-                        console.log(response.status);
                         return
                     }
                     // Examine the text in the response
                     response.json().then(function(data) {
 console.log('line 19');
-let temp =
+                        let optionValues = "";
                         data['data'].forEach(item => {
-                            console.log(item.option);
-                           temp = temp + '<p>item.option</p>'
+                            optionValues += `<option value="${item.id}">${item.option}</option>`
                         })
-                        document.querySelector('#stage-options').innerHTML = '<p>hello</p>' + '<p>fgjkhdhgkjdfhgkjd</p>'
-
-
-//                         displayField(data, 'id')
-//                         displayField(data, 'option')
-                        let x = data['data'];
-                        console.log(x[0]);
-                        console.log(data['data']);
-
+                        document.querySelector('#next-stage-options').innerHTML += optionValues
                     })
-                    console.log('hi');
                 }
             )
-
-        // $("#nextStageModal").modal()
     })
 })
