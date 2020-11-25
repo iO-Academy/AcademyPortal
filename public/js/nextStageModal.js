7361a781d6b8810080bc3dabeac9c0cf04e268b9
Davin <davin.hayden@outlook.com>
2020-11-25 12:17:56 +0000

    Added if/else so that modal only display options for the Next Stage if they are present, and  added semantic name for btnNextStage that loads this modal from DisplayApplicantViewHelper.php

--- public/js/nextStageModal.js
+++ public/js/nextStageModal.js
@@ -1,24 +1,33 @@
 
 $(document).ready(function(){
-    $(".fish").click(function(){
-        var url = './api/getNextStageOptions'
+    $(".btnNextStage").click(function(){
+        const stageId = this.dataset.stageid;
+        var url = './api/getNextStageOptions/' + stageId;
         fetch(url)
             .then(
                 function(response) {
                     if (response.status !== 200) {
-                        document.querySelector('#modal-main').innerHTML = ''
+                        document.querySelector('#modal-main').innerHTML = '';
                         document.querySelector('#modal-main').innerHTML += '<div class="alert alert-danger" role="alert">Looks like there was a problem. Status Code: ' +
-                            response.status + '</div>'
-                        return
+                            response.status + '</div>';
+                        return;
                     }
                     // Examine the text in the response
                     response.json().then(function(data) {
-console.log('line 19');
-                        let optionValues = "";
-                        data['data'].forEach(item => {
-                            optionValues += `<option value="${item.id}">${item.option}</option>`
-                        })
-                        document.querySelector('#next-stage-options').innerHTML += optionValues
+                        document.querySelector('#next-stage-options').innerHTML = '<option>Please select an Option</option>';
+                        //if there are no option for next stage, call another controller to move to next stage
+                        if (data['data']['nextStageOptions'].length === 0) {
+                            //call other controller
+                        }
+                        //else show options  for the next stage in a modal
+                        else {
+                            let optionValues = "";
+                            data['data']['nextStageOptions'].forEach(item => {
+                                optionValues += `<option value="${item.id}">${item.option}</option>`;
+                            })
+                            document.querySelector('#next-stage-options').innerHTML += optionValues;
+                            $('#nextStageModal').modal('show');
+                        }
                     })
                 }
             )
