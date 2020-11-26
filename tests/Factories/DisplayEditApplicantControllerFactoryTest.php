2dd3d689f9c3918ae7106eeae1e8753a5f027eab
Tom Hinsley <tomhinsley@me.com>
2020-11-26 13:22:10 +0000

    created DisplayEditApplicantControllerFactoryTest

---
+++ tests/Factories/DisplayEditApplicantControllerFactoryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Tests\Factories;
+
+use Portal\Controllers\FrontEnd\DisplayEditApplicantController;
+use Portal\Factories\DisplayEditApplicantControllerFactory;
+use Portal\Models\ApplicantModel;
+use Portal\Models\StageModel;
+use Psr\Container\ContainerInterface;
+use Tests\TestCase;
+
+class DisplayEditApplicantControllerFactoryTest extends TestCase
+{
+    public function testInvoke()
+    {
+        $container = $this->createMock(ContainerInterface::class);
+        $stage = $this->createMock(StageModel::class);
+        $applicant = $this->createMock(ApplicantModel::class);
+        $container->method('get')
+            ->willReturn($stage, $applicant);
+
+        $factory = new DisplayEditApplicantControllerFactory;
+        $case = $factory($container);
+        $expected = DisplayEditApplicantController::class;
+        $this->assertInstanceOf($expected, $applicant);
+
+    }
+}
