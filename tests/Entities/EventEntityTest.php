a7948e793c36efad6640e53ecf0d1037b2f9f10f
Jenny Allton-Twist <jennyalltontwist@gmail.com>
2020-11-26 13:23:32 +0000

    added constuctor success test and mandatory fields only success test

--- tests/Entities/EventEntityTest.php
+++ tests/Entities/EventEntityTest.php
@@ -177,4 +177,39 @@ class EventEntityTest extends TestCase
         $result = $name->getAvailableToHP();
         $this->assertEquals($result, 0);
     }
+
+    public function testConstructorFailure()
+    {
+        $this->expectException(Exception::class);
+        new EventEntity(
+            1,
+            'hiring eventhhhhfhfgsgshshshshshhshshshshhshshshjakhdkahfksdhflsahfkhaslfkhskfhlkhfklahfdfkhgkaldhfsf
+            ghdkfhgkdjfhgkadhglkjadfghkldfjhfgksdhgkljashglkashdgklahsdfglkjhaklghaskjghaskjhgdkasdhgkjhgkajhagdff
+            mhetjuertuergthjergtjerhwgtjehwrgtjewrgtjkewrgtjehrgtjkehgrytwerdgfdg',
+            2,
+            '1 Widcombe Cres, Bath BA2 6AH',
+            '2019-10-02',
+            '18:00',
+            '21:00',
+            'notes',
+            0
+        );
+    }
+
+    public function testMandatoryFieldsOnlySuccess()
+    {
+        $actual = new EventEntity(
+            1,
+            'hiring event',
+            2,
+            '1 Widcombe Cres, Bath BA2 6AH',
+            '2019-10-02',
+            '18:00',
+            '21:00'
+        );
+        $this->assertInstanceOf(EventEntity::class, $actual);
+    }
+
+
+
 }
