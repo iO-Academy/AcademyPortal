7627e583884c8aef7601c8684682e59acc30f931
HarryCusworth <harry.cusworth@protonmail.com>
2021-04-08 07:56:10 +0100

    fixed bug and unit test

--- tests/ViewHelpers/DisplayApplicantViewHelperTest.php
+++ tests/ViewHelpers/DisplayApplicantViewHelperTest.php
@@ -61,7 +61,7 @@ class DisplayApplicantViewHelperTest extends TestCase
     public function testSuccessDisplayApplicantsEmptyArray()
     {
         $data = ['applicants' => [], 'lastStage' => '9'];
-        $expected = '<tr><td colspan="5"><h5 class="text-danger text-center">No Applicants Found.</h5></td></tr>';
+        $expected = '<tr><td colspan="6"><h5 class="text-danger text-center">No Applicants Found.</h5></td></tr>';
         $result = DisplayApplicantViewHelper::displayApplicants($data);
         $this->assertEquals($expected, $result);
     }
