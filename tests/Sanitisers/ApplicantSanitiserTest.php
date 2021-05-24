086dee031b8ba26727bf188dab8a3e1aef303e09
Imy <imypullan@gmail.com>
2021-05-24 16:43:40 +0100

    adding new lines at end of tests to pass CI

---
+++ tests/Sanitisers/ApplicantSanitiserTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Validators;
+
+use Portal\Validators\ApplicantValidator;
+use Tests\TestCase;
+
+class ApplicantSanitiserTestTest extends TestCase
+{
+    public function testSanitiseStageOrders()
+    {
+        $this->markTestSkipped('Cannot unit test as method calls other methods');
+    }
+}
