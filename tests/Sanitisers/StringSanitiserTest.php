0370cab9236d885eaf1aa635896af03bc1d31f35
Imy <imypullan@gmail.com>
2021-05-24 15:25:52 +0100

    stringSanitiserTest success and failure

---
+++ tests/Sanitisers/StringSanitiserTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace Tests\Sanitisers;
+
+use Portal\Sanitisers\StringSanitiser;
+use Tests\TestCase;
+
+class StringSanitiserTest extends TestCase
+{
+    public function testSanitiseStringSuccess()
+    {
+        $str = '    <h1>Hello World!</h1>';
+        $result = StringSanitiser::sanitiseString($str);
+        $expected = 'Hello World!';
+        $this->assertEquals($result, $expected);
+    }
+    public function testSanitiseStringSuccessNull()
+    {
+        $result = StringSanitiser::sanitiseString(null);
+        $expected = '';
+        $this->assertEquals($result, $expected);
+    }
+    public function testSanitiseStringFailure()
+    {
+        $arr = [];
+        $this->expectException(\TypeError::class);
+        StringSanitiser::sanitiseString($arr);
+    }
+//    public function testSanitiseStringMalformed()
+//    {
+//
+//    }
+}
