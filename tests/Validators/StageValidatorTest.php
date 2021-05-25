0b2e467d6989959284d2668f0c99d4e09a4cd6a1
Maria Gusova <masha.gu@gmail.com>
2021-05-25 10:35:26 +0100

    add newStage test

--- tests/Validators/StageValidatorTest.php
+++ tests/Validators/StageValidatorTest.php
@@ -7,8 +7,15 @@ use Tests\TestCase;
 
 class StageValidatorTest extends TestCase
 {
-    public function testSanitiseStageOrders()
+    public function testValidateStage()
     {
         $this->markTestSkipped('Cannot unit test as method calls other methods');
+    }
+
+    public function testValidateNewStageSuccess()
+    {
+        $newStage = ['title' => 'New application', 'student' => false];
+        $result = StageValidator::validateNewStage($newStage);
+        $this->assertEquals($result, true);
     }
 }
