29efc16aaa69eaab5dd4389c0eb89f61621d6170
MattW-R <Matthew.wr89@gmail.com>
2021-05-26 14:09:26 +0100

    Added condition in applicantForm.js to prevent any attempt to access a taster dropdown on the new applicant page

--- public/js/applicantForm.js
+++ public/js/applicantForm.js
@@ -1,7 +1,9 @@
 (async () => {
     const data = await handleFormOptions();
     outputCohorts(data.cohorts);
-    outputEvents(data.tasters, document.getElementById('tasterDate'));
+    if (document.getElementById('tasterDate')) {
+        outputEvents(data.tasters, document.getElementById('tasterDate'));
+    }
     outputHearAbout(data.hearAbout);
     if (document.querySelector('#chosenCourseId')) {
         outputCohorts(data.cohorts, document.querySelector('#chosenCourseId'));
