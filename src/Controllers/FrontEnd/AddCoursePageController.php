723a3ef391533e0995b2432060580362bf51e1b3
Mike Oram <mike.oram@mayden.co.uk>
2021-07-23 16:56:10 +0100

    Update AddCoursePageController.php

--- src/Controllers/FrontEnd/AddCoursePageController.php
+++ src/Controllers/FrontEnd/AddCoursePageController.php
@@ -25,4 +25,22 @@ class AddCoursePageController extends Controller
         $this->courseModel = $courseModel;
     }
 
-
+    /**
+     * Checks for logged-in status,
+     * gets 
+     * and returns rendered landing screen for Courses page
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     * @return Response
+     */
+    public function __invoke(Request $request, Response $response, array $args)
+    {
+        if ($_SESSION['loggedIn'] === true) {
+            return $this->renderer->render($response, 'addCourse.phtml', $args);
+        } else {
+            return $response->withHeader('Location', './');
+        }
+    }
+}
