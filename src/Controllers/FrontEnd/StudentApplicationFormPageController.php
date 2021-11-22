0c0fa0625a81f74ef9f9fe8e835d59c25ce9f306
Jake Rooms <jakeprooms@outlook.com>
2021-11-22 12:00:57 +0000

    Added a StudentApplicationFormPageController to the frontEnd controllers. This links to the ApplicationFormModel.

---
+++ src/Controllers/FrontEnd/StudentApplicationFormPageController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Portal\Controllers\FrontEnd;
+
+use Portal\Models\ApplicationFormModel;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Slim\Views\PhpRenderer;
+
+class StudentApplicationFormPageController
+{
+    protected ApplicationFormModel $model;
+    protected PhpRenderer $view;
+
+    /**
+     * @param ApplicationFormModel $model
+     * @param PhpRenderer $view
+     */
+    public function __construct(ApplicationFormModel $model, PhpRenderer $view)
+    {
+        $this->model = $model;
+        $this->view = $view;
+    }
+
+    public function __invoke(RequestInterface $request,ResponseInterface $response,array $args): ResponseInterface
+    {
+        $data = [
+            'backgroundInfo' => $this->model->getBackgroundInfo(),
+            'hearAbout' => $this->model->getHearAbout(),
+        ];
+
+        return $this->view->render($response, 'studentApplicationForm.phtml', $data);
+    }
+
+
+}
