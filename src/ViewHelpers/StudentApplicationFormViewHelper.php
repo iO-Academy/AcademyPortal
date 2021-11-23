02be4cddc875bb4e3512d2be05b13757f0fca29e
Jake Rooms <jakeprooms@outlook.com>
2021-11-23 11:53:08 +0000

    added dropdown and correct form fields to the view helper methods

--- src/ViewHelpers/StudentApplicationFormViewHelper.php
+++ src/ViewHelpers/StudentApplicationFormViewHelper.php
@@ -15,8 +15,12 @@ class StudentApplicationFormViewHelper
         $output = '<input type="text" placeholder="Full Name">';
         $output .= '<input type="email" placeholder="Email">';
         $output .= '<input type="tel" placeholder="Phone Number">';
-        // REPLACE WITH A DROPDOWN --> separate component for gender drop down options?
-        $output .= '<label>Gender </label>';
+        $output .= '<select>';
+        $output .= '<option>Gender</option>';
+        foreach($data['genders'] as $genders){
+            $output .= '<option value="' . $genders['id'] . '">' . $genders['gender'] . '</option>';
+        }
+        $output .= '</select>';
         return $output;
     }
 
@@ -28,10 +32,14 @@ class StudentApplicationFormViewHelper
      */
     protected static function displayPageFormTwo(array $data): string
     {
-        // REPLACE WITH A DROPDOWN --> fetch background from DB and add to populateDropdown
-        $output = '<label>Background </label>';
+        $output = '<select>';
+        $output .= '<option>Background</option>';
+        foreach($data['backgroundInfo'] as $backgroundInfo){
+            $output .= '<option value="' . $backgroundInfo['id'] . '">' . $backgroundInfo['backgroundInfo'] . '</option>';
+        }
+        $output .= '</select>';
         $output .= '<label>Why do you want to become a developer?';
-        $output .= '<input type="text" placeholder="(100 - 500 characters)">';
+        $output = '<textarea rows="10" placeholder="(100-500 Characters)"> </textarea>';
         $output .= '</label>';
         return $output;
     }
@@ -43,8 +51,8 @@ class StudentApplicationFormViewHelper
      */
     protected static function displayPageFormThree(): string
     {
-        $output = '<label>Any past coding experience?';
-        $output .= '<input type="text" placeholder="Most people write a few sentences">';
+        $output = '<label>Any past coding experience?</label>';
+        $output = '<textarea rows="10" placeholder="Most people write a few sentences"> </textarea>';
         $output .= '</label>';
         return $output;
     }
@@ -57,21 +65,25 @@ class StudentApplicationFormViewHelper
      */
     protected static function displayPageFormFour(array $data): string
     {
-        $output = '<label>Select start date(s)';
-        //get cohorts from DB and for each
-        $output .= '<input type="checkbox">';
-        $output .= '<input type="checkbox" value="next available online course">';
-        $output .= '<p>Some course dates may also be offered with a remote option. Contact us to find out more.</p>';
-        $output .= '<label> How did you hear about us?';
-        // REPLACE WITH A DROPDOWN - - > hear about us dropdown list.
-        $output .= '<label> Course Report </label>';
-        $output .= '<input type="radio" value="I am eligible to live and work in the UK"/>';
-        $output .= '<input type="radio" value="I confirm that I am at least 18 years 
-of age before my chosen course start date"/>';
-        $output .= '<input type="radio" value="I am eligible to live and work in the UK"/>';
+        $output = '<label>Select start date(s)</label>';
+        foreach($data['cohorts'] as $cohorts){
+            $output .= '<label><input type="checkbox" value="' . $cohorts['id'] . '"/>' . $cohorts['date'] . '</label>';
+        }
+        $output .= '<label><input type="checkbox" value="next available online course">Some course dates may also be offered with a remote option. Contact us to find out more.</label>';
+        $output .= '<label> How did you hear about us?</label>';
+        $output .= '<select>';
+        $output .= '<option>Background</option>';
+        foreach($data['hearAbout'] as $hearAbout){
+            $output .= '<option value="' . $hearAbout['id'] . '">' . $hearAbout['hearAbout'] . '</option>';
+        }
+        $output .= '</select>';
+        $output .= '<label><input type="checkbox" value="I am eligible to live and work in the UK"/>I am eligible to live and work in the UK</label>';
+        $output .= '<label><input type="checkbox" value="I confirm that I am at least 18 years 
+of age before my chosen course start date"/>I confirm that I am at least 18 years 
+of age before my chosen course start date<label>';
         $output .= '<p>By using this form you agree with the storage and handling of your data
  by this website in accordance with our terms and conditions and privacy policy.</p>';
-        $output .= '<input type="radio" value="I accept the terms and conditions"/>';
+        $output .= '<label><input type="checkbox" value="I accept the terms and conditions"/>I accept the terms and conditions</label>';
         return $output;
     }
 
