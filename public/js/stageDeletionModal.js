4e493d6536e03cc10526469dce96eddc1dcde83b
DevUsman-18 <mohammad_usman117@hotmail.co.uk>
2021-11-24 12:28:33 +0000

    added and styled modal, with close modal functionality

---
+++ public/js/stageDeletionModal.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#stageDeletionModalYes").click(function(){
+        // add functionality to the Yes button
+        $('#stageDeletionModal').modal('hide');
+    })
+
+    $("#stageDeletionModalCancel").click(function(){
+        $('#stageDeletionModal').modal('hide');
+    })
+})
+
