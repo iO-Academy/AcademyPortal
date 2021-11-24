09f609b21eb556cb6c185a3849771438e392556c
Reductito <damianbell96@gmail.com>
2021-11-24 10:04:32 +0000

    Fixed indentation and spacing

--- src/ViewHelpers/StagesPageViewHelper.php
+++ src/ViewHelpers/StagesPageViewHelper.php
@@ -89,12 +89,12 @@ class StagesPageViewHelper
 //                    $result .= '<td class="col-xs-2 text-center disabled"><a data-id="' . $stage->getStageId()
 //                    . '" class="text-danger delete disabled">Delete</a></td>';
 //                }
-                $result .= '<td class="col-xs-1 text-center disabled"><a data-id="' . $stage->getStageId()
-                        . '" class="text-danger delete disabled">Delete</a></td>';
+                $result .= '<td class="col-xs-1 text-center disabled"><a data-id="' . $stage->getStageId() . '" class="text-danger delete disabled">Delete</a></td>';
                 $result .= '<td class="col-xs-1 text-center">';
                 if (empty($stage->getOptions())) {
-                $result .= '<i data-stageId="' . $stage->getStageId() . '" data-locked="1" class="stageLock glyphicon glyphicon-lock text-success"></i>';
-                } else {
+                $result .= '<i data-stageId="' . $stage->getStageId() . '" data-locked="1" 
+                class="stageLock glyphicon glyphicon-lock text-success"></i>';
+                    } else {
                     $result .= '<img data-stageId="' . $stage->getStageId() . '" data-locked="1" class="stageLock text-success" src="../../assets/reshot-icon-lock-disabled-9TKYNWASVB.svg" />';
                 }
                 $result .= '</td>';
