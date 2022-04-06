6a4e5fdad564485e8f717149260723056d9572ee
“Maxwell <newton.maxwell@gmail.com>
2022-04-06 10:57:25 +0100

    removed excess spacing

--- src/Controllers/FrontEnd/ApplicantsPageController.php
+++ src/Controllers/FrontEnd/ApplicantsPageController.php
@@ -73,13 +73,8 @@ class ApplicantsPageController extends Controller
                     $params['cohortId'],
                     $params['sort']
                 );
-
             $params['count'] = ceil(count($allApplicants) / $this->numberOfApplicantsPerPage); // counts number of pages
-
-            if (isset($_SESSION['page']) && ($_SESSION['page'] > $params['count'] || $_SESSION['page'] < 1)) {
-                $_SESSION['page'] = 1;
-            }
-
+            if (isset($_SESSION['page']) && ($_SESSION['page'] > $params['count'] || $_SESSION['page'] < 1)) {$_SESSION['page'] = 1;}
             $params['data']['applicants'] = array_slice($allApplicants, ($params['page'] - 1) * $this->numberOfApplicantsPerPage , $this->numberOfApplicantsPerPage);
 
             return $this->renderer->render($response, 'applicants.phtml', $params);
