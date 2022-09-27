45cd052f21ca915f7bf3ddc17fe7491f3578328c
josh-lew <joshlewisdev@gmail.com>
2022-09-27 10:08:54 +0100

    addNewTrainer() and deleteTrainer() functions added with docblocks

--- src/Models/TrainerModel.php
+++ src/Models/TrainerModel.php
@@ -31,4 +31,41 @@ class TrainerModel
         $query->execute();
         return $query->fetchAll();
     }
+
+    /**
+     * Add a new trainer to a database
+     *
+     * @param \PDO $db
+     * @param string $name
+     * @param string $email
+     * @param string $notes
+     * @return boolean
+     */
+    public function addNewTrainer(\PDO $db, $name, $email, $notes): bool
+    {
+        $query = $this->db->prepare(
+            "INSERT INTO `trainers` (`name`, `email`, `notes`)
+            VALUES (:name, :email, :notes);"
+        );
+        $query->bindParam(':name', $name);
+        $query->bindParam(':email', $email);
+        $query->bindParam(':notes', $notes);
+        return $query->execute();
+    }
+
+    /**
+     * Updates deleted field to 1
+     *
+     * @param \PDO $db
+     * @param string $id
+     * @return boolean
+     */
+    public function deleteTrainer(\PDO $db, $id): bool
+    {
+        $query = $this->db->prepare(
+            "UPDATE `trainers` SET `deleted` = 1 WHERE `id` = :id;"
+        );
+        $query->bindParam(':id', $id);
+        return $query->execute();
+    }
 }
