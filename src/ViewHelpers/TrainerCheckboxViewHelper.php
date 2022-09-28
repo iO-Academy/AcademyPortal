620fffb057bd656e05dae2069f1b498ff53bd147
josh-lew <joshlewisdev@gmail.com>
2022-09-28 16:42:33 +0100

    TrainerCheckboxViwHelper function completed with docblock

--- src/ViewHelpers/TrainerCheckboxViewHelper.php
+++ src/ViewHelpers/TrainerCheckboxViewHelper.php
@@ -4,13 +4,19 @@ namespace Portal\ViewHelpers;
 
 class TrainerCheckboxViewHelper
 {
-    public static function displayTrainerCheckbox(array $trainers)
+    /**
+     * take list of trainers and foreach to display them as individual checkbox items
+     *
+     * @param array $trainers
+     * @return string
+     */
+    public static function displayTrainerCheckbox(array $trainers): string
     {
         $result = "";
 
         foreach ($trainers as $trainer) {
             $result .= '<div><input type="checkbox" data-id="' . $trainer->getId() . '" name="trainer-checkbox"';
             $result .= ' checked><label for="">' . $trainer->getName() . '</label></div>';
-        }
+        } return $result;
     }
 }
