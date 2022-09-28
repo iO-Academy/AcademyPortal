3b05a1449714eaccb55ade7c76c124227d588dd0
aidanmaskell <aidan.maskell@gmail.com>
2022-09-28 11:22:31 +0100

    added to eventhandler in trainermodal js file, WIP

--- public/js/trainernotesmodal.js
+++ public/js/trainernotesmodal.js
@@ -1,6 +1,9 @@
 let trainerNames = document.querySelectorAll('.myBtn')
 trainerNames.forEach(trainer => {
     trainer.addEventListener('click' , (e) => {
-        
+        document.querySelector('#name').textContent = e.target.dataset.name
+        console.log(e.target.dataset.name)
+        document.querySelector('#notes').textContent = e.target.dataset.notes
+        document.querySelector('#trainerNotesModal').modal()
     })
 })
