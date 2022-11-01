c97bb1d16030fb1450514dfd8dbca322c35eee97
Ashley Coles <ashley@acoles.co.uk>
2022-11-01 11:24:54 +0000

    Tidying validators

--- src/Validators/OptionsValidator.php
+++ src/Validators/OptionsValidator.php
@@ -7,8 +7,6 @@ class OptionsValidator
     /**
      * Takes a new option and validates the stageId exists, and is a number, and that optionTitle has content.
      * If all pass, returns true.
-     * @param array $option
-     * @return bool
      */
     public static function validateOption(array $option): bool
     {
--- src/Validators/StringValidator.php
+++ src/Validators/StringValidator.php
@@ -2,6 +2,8 @@
 
 namespace Portal\Validators;
 
+use Exception;
+
 class StringValidator
 {
     public const MAXVARCHARLENGTH = 255;
@@ -11,11 +13,7 @@ class StringValidator
     /**
      * Validate that a string exists and is within length allowed, throws an error if not
      *
-     * @param string $validateData
-     * @param int $characterLength
-     * @param string $fieldName
-     * @return string, which will return the validateData
-     * @throws \Exception if the array is empty
+     * @throws Exception if the array is empty
      */
     public static function validateExistsAndLength(
         string $validateData,
@@ -25,7 +23,7 @@ class StringValidator
         if (empty($validateData) == false && strlen($validateData) <= $characterLength) {
             return $validateData;
         } else {
-            throw new \Exception('You have either not inputted any information for ' . $fieldName . ' or ' . '
+            throw new Exception('You have either not inputted any information for ' . $fieldName . ' or ' . '
             it exceeds our character limits');
         }
     }
@@ -33,12 +31,7 @@ class StringValidator
     /**
      * Validate that a string is not empty and is within length allowed, throws an error if not
      *
-     * @param string $validateData
-     * @param int $characterLength
-     * @param string $fieldName
-     * @return bool which will return the validateData or assigns to null
-     * @throws \Exception if the data exceeds max length
-     *
+     * @throws Exception if the data exceeds max length
      */
     public static function validateLength(
         string $validateData,
@@ -46,7 +39,7 @@ class StringValidator
         string $fieldName = 'Unknown'
     ): bool {
         if (strlen($validateData) > $characterLength) {
-            throw new \Exception('You have either not inputted any information for ' . $fieldName . ' or ' . '
+            throw new Exception('You have either not inputted any information for ' . $fieldName . ' or ' . '
             it exceeds our character limits');
         }
         return true;
@@ -57,14 +50,14 @@ class StringValidator
      *
      * @param string $alpha
      * @return string
-     * @throws \Exception
+     * @throws Exception
      */
     public static function validateAlpha(string $alpha): string
     {
         if (preg_match(self::ALPHAREGEXPATTERN, $alpha)) {
             return $alpha;
         } else {
-            throw new \Exception('Please use alpha characters only');
+            throw new Exception('Please use alpha characters only');
         }
     }
 }
