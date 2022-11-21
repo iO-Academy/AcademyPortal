f2fba0b26ed765d102866109a54527760b2927d9
owen <may.owend@gmail.com>
2022-11-21 14:34:18 +0000

    completed register user error message consistency

--- public/js/registerUser.js
+++ public/js/registerUser.js
@@ -31,41 +31,27 @@ newUserForm.addEventListener('submit', async (e) => {
     let data = getInputs(),
         messageBox = document.getElementById('message'),
         button = document.getElementById('newUserSubmit'),
-        response = await sendNewUserDetails(data),
         emailError = document.querySelector('#emailError')
 
-    console.log(validateEmailInputs(data.email));
-
-    messageBox.innerText = response['msg']
-    if (response['success'] && validateEmailInputs(data.email)) {
-        messageBox.classList.remove('alert-danger')
-        messageBox.classList.add('alert-success')
-        messageBox.innerText += '. A new password will be generated in 5 seconds.';
-        setTimeout(() => {
-            window.location.reload()
-        }, 5000)
-    } else {
-        messageBox.classList.remove('alert-success')
-        messageBox.classList.add('alert-danger')
+    if (validateEmailInputs(data.email)) {
+            let response = await sendNewUserDetails(data)
+            messageBox.innerText = response['msg']
+            if (response['success']) {
+                messageBox.classList.remove('alert-danger')
+                messageBox.classList.add('alert-success')
+                messageBox.innerText += '. A new password will be generated in 5 seconds.'
+                emailError.classList.remove('alert-danger')
+                emailError.classList.add('hidden')
+                setTimeout(() => {
+                    window.location.reload()
+                }, 5000)
+            } else {
+                messageBox.classList.remove('alert-success')
+                messageBox.classList.add('alert-danger')
+            }
+        } else {
         emailError.innerText = "This doesn't appear to be a valid email address. Please try again.";
         emailError.classList.add('alert-danger');
         emailError.classList.remove('hidden');
     }
 })
-
-
-// Copy and Paste Code from addTrainer.js for validating email and displaying error
-
-let validateEmailInputs = (email) => {
-    return varCharMaxLength(email) && isEmail(email) && isPresent(email);
-};
-
-let errorMessage = (validationType) => {
-    let htmlString = '';
-    switch (validationType) {
-        case 'isEmail' :
-            htmlString += `This doesn't appear to be a valid email address. Please try again.`;
-            break;
-    }
-return htmlString;
-};
--- public/js/utils.js
+++ public/js/utils.js
@@ -1,7 +1,11 @@
-export const formSubmitSuccess = () => {
+const formSubmitSuccess = () => {
     message.classList.add('alert-success');
     message.classList.remove('alert-danger');
     setTimeout(() => {
         window.location.reload();
     }, 5000);
 }
+
+const validateEmailInputs = (email) => {
+    return varCharMaxLength(email) && isEmail(email) && isPresent(email);
+};
