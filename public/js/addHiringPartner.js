447ac9a98ce9bd61fc1f73f73a0f3619a5d25b10
owen <may.owend@gmail.com>
2022-11-23 14:06:43 +0000

    addhiringpartner.js and view have been edited so that all error messages are consistent.

--- public/js/addHiringPartner.js
+++ public/js/addHiringPartner.js
@@ -1,133 +1,148 @@
 const addHiringPartnerForm = document.querySelector('#add-hiring-partner-form')
+const message = document.querySelector('#messages');
 
-addHiringPartnerForm.addEventListener('submit', async e => {
+// Submit Form + Add New Event API Call
+addHiringPartnerForm.addEventListener("submit", e => {
     e.preventDefault()
+    const errorDivs = document.querySelectorAll('.alert');
+    errorDivs.forEach(errorDiv => {
+        errorDiv.classList.add('hidden');
+    })
 
-    let data = {
-        name: addHiringPartnerForm['company-name'].value.trim(),
-        companySize: addHiringPartnerForm['company-size'].value,
-        techStack: addHiringPartnerForm['company-tech-stack'].value.trim(),
-        postcode: addHiringPartnerForm['company-postcode'].value.trim(),
-        phoneNumber: addHiringPartnerForm['company-phone-number'].value.trim(),
-        companyUrl: addHiringPartnerForm['company-url'].value.trim()
-    }
 
-    if( validateHiringPartnerForm(data) ) {
-        await addHiringPartner(data)
-        getHiringPartners()
+    let data = getCompletedFormData();
+    let validatedFormItems = validateHpInputs(data);
+    let formIsValid = true;
+
+    Object.keys(validatedFormItems).forEach(formItemKey => {
+        const errorDiv = document.querySelector(`#${formItemKey}Error`);
+        let formItemValues = validatedFormItems[formItemKey];
+        let keys = Object.keys(formItemValues);
+        for (let i = 0; i < keys.length; i++) {
+            let key = keys[i];
+            let isValid = formItemValues[key];
+            if (!isValid) {
+                errorDiv.classList.add('alert-danger');
+                errorDiv.classList.remove('hidden');
+                errorDiv.innerHTML = errorMessage(key);
+                formIsValid = false;
+                message.classList.add('hidden')
+                break;
+            }
+        }
+    });
+
+    if (formIsValid) {
+        // send it!
+        fetch('./api/createHiringPartner', {
+            credentials: 'same-origin',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            method: 'post',
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then(responseJson => {
+                if (responseJson.success) {
+                    addHiringPartnerForm.elements['company-name'].value = '',
+                    addHiringPartnerForm.elements['company-size'].value = '',
+                    addHiringPartnerForm.elements['company-tech-stack'].value = '',
+                    addHiringPartnerForm.elements['company-postcode'].value = '',
+                    addHiringPartnerForm.elements['company-phone-number'].value = '',
+                    addHiringPartnerForm.elements['company-url'].value = ''
+                    message.innerText = responseJson.message
+                    message.classList.add('alert-success')
+                    message.classList.remove('alert-danger')
+                } else {
+                    message.innerText = responseJson.message
+                    message.classList.add('alert-danger')
+                    message.classList.remove('alert-success')
+                }
+            })
     }
 })
 
-function validateHiringPartnerForm(data) {
-    let message = ''
+/**
+ * Adds data from form into an object with the field name as key and the form value as value.
+ */
 
-    let validateCompanyName = function (name) {
-        if (name.length < 1) {
-            message += 'Company Name is a required field!<br>'
-            return false
-        }
-        if (name.length > 255) {
-            message += 'Company Name is too long!<br>'
-            return false
-        }
-        return true
-    }(data.name)
-
-    let validateCompanySize = function (size) {
-        if (size == 0) {
-            message += 'Company Size is a required field!<br>'
-            return false
-        }
-        let companySizes = document.querySelector('#company-size').childElementCount
-        if (size > companySizes) {
-            message += 'Invalid company size option!<br>'
-            return false
-        }
-        return true
-    }(data.companySize)
-
-    let validateTechStack = function (techStack) {
-        if (techStack.length < 1) {
-            message += 'Company Tech Stack is a required field!<br>'
-            return false
-        }
-        if (techStack.length > 600) {
-            message += 'Company Tech Stack is too long!<br>'
-            return false
-        }
-        return true;
-    }(data.techStack)
-
-    let validatePostcode = function (postcode) {
-        if (postcode.length < 1) {
-            message += 'Company Postcode is a required field!<br>'
-            return false
-        }
-        if (postcode.length > 10) {
-            message += 'Company Postcode is too long!<br>'
-            return false
-        }
-        if (!isPostcode(postcode)) {
-            message += 'Invalid postcode format!<br>'
-            success = false
-        }
-        return true
-    }(data.postcode)
-
-    let validatePhoneNumber = function (phone) {
-        if (phone.length > 20) {
-            message += 'Company Phone Number is too long!<br>'
-            return false
-        }
-        if (!isPhoneNumber(phone)) {
-            message += 'Invalid Phone Number format!<br>'
-            return false
-        }
-        return true
-    }(data.phoneNumber)
-
-    let validateUrl = function (url) {
-        if (url.length > 255) {
-            message += 'Company Website URL is too long!<br>'
-            return false
-        }
-        if (!isUrl(url)) {
-            message += 'Invalid company URL!<br>'
-            return false
-        }
-        return true
-    }(data.companyUrl)
-
-    document.getElementById('add-hiring-partner-messages').innerHTML = message
-    if( validateCompanyName 
-        && validateCompanySize 
-        && validateTechStack 
-        && validatePostcode 
-        && validatePhoneNumber 
-        && validateUrl ){
-        return true
-    } else {
-        return false
+let getCompletedFormData = () => {
+    let data = {
+        companyName: addHiringPartnerForm.elements['company-name'].value,
+        companySize: addHiringPartnerForm.elements['company-size'].value,
+        techStack: addHiringPartnerForm.elements['company-tech-stack'].value,
+        postcode: addHiringPartnerForm.elements['company-postcode'].value,
+        phoneNumber: addHiringPartnerForm.elements['company-phone-number'].value,
+        url: addHiringPartnerForm.elements['company-url'].value
     }
+
+    return data
 }
 
-let addHiringPartner = async data => {
-    fetch('./api/createHiringPartner', {
-        credentials: 'same-origin',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
+
+let validateHpInputs = (data) => {
+    validate = {
+        companyName: {
+            isPresent: isPresent(data.companyName),
+            isName: isName(data.companyName),
+            validLengthVarChar: varCharMaxLength(data.companyName)
         },
-        method: 'post',
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then((data) => {
-        if (data.success) {
-            document.getElementById('add-hiring-partner-form').reset()
-            document.getElementById('add-hiring-partner-messages').innerHTML = '<p>Hiring Partner successfully added.</p>'
-        } else {
-            document.getElementById('add-hiring-partner-messages').innerHTML = '<p>Hiring Partner not added.</p>'
-        }
-    })
+        companySize: {
+            isPresent: isPresent(data.companySize)
+        },
+        techStack: {
+            isPresent: isPresent(data.techStack),
+            isName: isName(data.techStack),
+            validLengthVarChar: varCharMaxLength(data.techStack)
+        },
+        postcode: {
+            isPresent: isPresent(data.postcode),
+            isPostcode: isPostcode(data.postcode),
+            validLengthVarChar: varCharMaxLength(data.postcode)
+        },
+        phoneNumber: {
+            isPresent: isPresent(data.phoneNumber),
+            isPhoneNumber: isPhoneNumber(data.phoneNumber),
+            validLengthVarChar: varCharMaxLength(data.phoneNumber)
+        },
+        url: {
+            isPresent: isPresent(data.url),
+            isUrl: isUrl(data.url),
+            validTime: isUrl(data.url)
+        },
+    };
+    return validate;
+};
+
+let errorMessage = (validationType) => {
+    let htmlString = '';
+
+    switch (validationType) {
+        case 'isPresent':
+            htmlString = `This field must be filled in.`;
+            break;
+        case 'validLengthVarChar':
+            htmlString = `This field must be less than 255 characters.`;
+            break;
+        case 'validLengthText':
+            htmlString = `This field must be less than 5000 characters.`;
+            break;
+        case 'isName':
+            htmlString = `Please use alphanumeric characters only.`;
+            break;
+        case 'isPostcode':
+            htmlString = `Please enter a valid postcode.`;
+            break;
+        case 'isPhoneNumber':
+            htmlString = `Please enter a valid phone number.`;
+            break;
+        case 'isUrl':
+            htmlString = 'Please enter a valid URL.'
+        default:
+            htmlString = `This field is invalid.`;
+            break;
+    }
+    return htmlString;
 }
