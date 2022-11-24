429c20e9515ee7747df56f582cff501745e5a78e
Henry <henryppercy@gmail.com>
2022-11-24 15:38:36 +0000

    Added database connection and calculations for percentage and totals

--- src/Models/ApplicantReportModel.php
+++ src/Models/ApplicantReportModel.php
@@ -13,56 +13,78 @@ class ApplicantReportModel
         $this->db = $db;
     }
 
-    public function getGenderReport(): array
+    public function extractDataForApplicantReports(int $reportType, int $startDate, int $endDate): array
     {
-        $genderData = [
-            ['title' => 'Gender'],
-            ['total' => 20],
-            ['rows' =>
-                [
-                    ['category' => 'Male', 'total' => 10, 'percentage' => 50],
-                    ['category' => 'Female', 'total' => 10, 'percentage' => 50],
-                    ['category' => 'Non-binary', 'total' => 0, 'percentage' => 0],
-                    ['category' => 'Prefer not to say', 'total' => 0, 'percentage' => 0],
-                ]
-            ]
-        ];
+        switch ($reportType) {
+            case 1:
+                $title = 'Gender';
+                $report = 'gender';
+                $column = '`gender`.`gender`';
+                $table = 'gender';
+                $foreignKeyColumn = 'gender';
+                break;
+            case 2:
+                $title = 'Background';
+                $report = 'backgroundInfo';
+                $column = 'backgroundInfo';
+                $table = 'background_info';
+                $foreignKeyColumn = 'backgroundInfoId';
+                break;
+            case 3:
+                $title = 'Hear About Us';
+                $report = 'hearAbout';
+                $column = 'hearAbout';
+                $table = 'hear_about';
+                $foreignKeyColumn = 'hearAboutId';
+                break;
+            default:
+                $title = '';
+                $report = '';
+                $column = '';
+                $table = '';
+                $foreignKeyColumn = '';
+        }
 
-        return $genderData;
-    }
+        $sql = 'SELECT ' . $column . ' AS "report", COUNT(' . $column . ') AS "total"
+                FROM `applicants`
+                LEFT JOIN `' . $table . '`
+                ON `applicants`.`' . $foreignKeyColumn . '` = `' . $table . '`.`id`
+                WHERE `dateTimeAdded` BETWEEN :startDate AND :endDate
+                GROUP BY 1
+                UNION
+                SELECT `' . $report . '`, 0 AS "total"
+                FROM `' . $table . '`
+                WHERE `' . $table . '`.`id` NOT IN (
+                SELECT DISTINCT `applicants`.`' . $foreignKeyColumn . '`
+                FROM `applicants`
+                WHERE `dateTimeAdded` BETWEEN :startDate AND :endDate)
+                GROUP BY 1 ;';
 
-    public function getBackgroundReport(): array
-    {
-        $backgroundData = [
-            ['title' => 'Background'],
-            ['total' => 20],
-            ['rows' =>
-                [
-                    ['category' => 'Student', 'total' => 10, 'percentage' => 50],
-                    ['category' => 'Not Student', 'total' => 10, 'percentage' => 50],
-                    ['category' => 'Career Switch', 'total' => 0, 'percentage' => 0],
-                    ['category' => 'Prefer not to say', 'total' => 0, 'percentage' => 0],
-                ]
-            ]
-        ];
 
-        return $backgroundData;
-    }
+        $query = $this->db->prepare($sql);
 
-    public function getHeadAboutUsReport(): array
-    {
-        $heardAboutUSData = [
-            ['title' => 'Heard About Us'],
-            ['total' => 20],
-            ['rows' =>
-                [
-                    ['category' => 'E-Mail', 'total' => 10, 'percentage' => 50],
-                    ['category' => 'Phone', 'total' => 10, 'percentage' => 50],
-                    ['category' => 'Word of the lord', 'total' => 0, 'percentage' => 0],
-                    ['category' => 'Prefer not to say', 'total' => 0, 'percentage' => 0],
-                ]
-            ]
-        ];
-        return $heardAboutUSData;
+        $query->bindParam(':startDate', $startDate);
+        $query->bindParam(':endDate', $endDate);
+        $query->execute();
+        $fetchedQuery = $query->fetchAll();;
+
+        $total = 0;
+
+        foreach ($fetchedQuery as $entry) {
+            $total += $entry['total'];
+        }
+
+        for ($i = 0; $i < count($fetchedQuery); $i++) {
+            $percentage = number_format(($fetchedQuery[$i]['total']/$total)*100, 0);
+            $fetchedQuery[$i][] = [$percentage];
+        }
+
+        $titleTotalArray[] = $title;
+        $titleTotalArray[] = $total;
+
+        $formattedReportData[] = [$titleTotalArray];
+        $formattedReportData[] = [$fetchedQuery];
+
+        return $formattedReportData;
     }
 }
