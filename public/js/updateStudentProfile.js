4eebe02748abfd18b8d8f57b4ad804b5328c3b28
Chloe Smart <chloesmartprint@hotmail.com>
2023-04-05 10:34:56 +0100

    made ajax functionality

--- public/js/updateStudentProfile.js
+++ public/js/updateStudentProfile.js
@@ -2,6 +2,8 @@ const edaidEditButton = document.querySelector('.edaidEditButton')
 const upfrontEditButton = document.querySelector('.upfrontEditButton')
 const laptopEditButton = document.querySelector('.laptopEditButton')
 const githubUserEditButton = document.querySelector('.githubUserEditButton')
+// please delete this line
+const studentId = 24
 
 function handleEditClick(event) {
     const buttonName = event.target.getAttribute('class')
@@ -9,14 +11,50 @@ function handleEditClick(event) {
     const containerDiv = document.querySelector('.' + divName)
     const fieldName = divName.replace('Container', '')
     containerDiv.innerHTML = 
-        '<form method="post">' +
+        '<form class="form" method="post">' +
         '<label for="' + fieldName + 'TextBox">' + containerDiv.childNodes[0].textContent + '</label>' +
         '<input type="text" id="' + fieldName + 'TextBox" name="' + fieldName + '">' + 
-        '<input type="submit" value="Save">' +
+        '<input class="saveButton" type="submit" value="Save">' +
         '</form>'
+    const saveButton = document.querySelector('.saveButton')
+    const form = document.querySelector('.form')
+
+    form.addEventListener('submit', (event) => {
+        const data = new FormData(form, saveButton)
+        const formData = data.get(fieldName)
+        event.preventDefault()
+        const updatedField = {
+            [fieldName]: formData,
+            id: studentId 
+        } 
+        const jsonUpdatedField = JSON.stringify(updatedField)
+        fetch('/api/updateStudentProfile', {
+            method: 'POST',
+            body: jsonUpdatedField,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            return response.json()
+        }).then((data) => {
+            if(response.status == 200) {
+                header('Location: studentProfile.phtml')
+            } else {
+                const responseMessage = response.statusText
+                console.log(responseMessage)
+            }
+        })
+    })
+
 }
 
+// function handleSaveClick(event) {
+
+// }
+
+
 edaidEditButton.addEventListener('click', handleEditClick)
+// saveButton.addEventListener('click', handleSaveClick)
 
 // UNCOMMENT THESE WHEN VIEWHELPER.PHP IS UPDATED WITH BUTTON ETC.
 // upfrontEditButton.addEventListener('click', handleEditClick)
