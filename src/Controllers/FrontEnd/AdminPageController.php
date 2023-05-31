f0b09c596c3cb0b89c02bb23c5011576a4779435
Phildallen <phil_372@live.com>
2023-05-31 12:16:49 +0100

    removed verbose debugger function

--- src/Controllers/FrontEnd/AdminPageController.php
+++ src/Controllers/FrontEnd/AdminPageController.php
@@ -28,7 +28,6 @@ class AdminPageController extends Controller
         $mail = new PHPMailer(true);
         try {
             // Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
             $mail->isSMTP();                                            // Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
