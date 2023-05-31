7590ae5e621848bb3af4b46e6a5275353c14a3b8
Phildallen <phil_372@live.com>
2023-05-31 12:07:45 +0100

    email now sends

--- src/Controllers/FrontEnd/AdminPageController.php
+++ src/Controllers/FrontEnd/AdminPageController.php
@@ -26,29 +26,24 @@ class AdminPageController extends Controller
     {
 
         $mail = new PHPMailer(true);
-
         try {
-            //Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-            $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'testacademyportal@gmail.com';                     //SMTP username
-            $mail->Password   = 'nickloveslemons';                               //SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-            //Recipients
+            // Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = 'testacademyportal@gmail.com';         // SMTP username
+            $mail->Password   = 'mnhftppxpklunjug';                       // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;          // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+            $mail->Port       = 587;                                    // TCP port to connect to
+            // Recipients
             $mail->setFrom('from@example.com', 'Mailer');
-            $mail->addAddress('testacademyportal@gmail.com', 'test');     //Add a recipient
-
-
-            //Content
-            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->addAddress('testacademyportal@gmail.com', 'test');  // Add a recipient
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
             $mail->Subject = 'Here is the subject';
             $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
             $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
             $mail->send();
             echo 'Message has been sent';
         } catch (Exception $e) {
