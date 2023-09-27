32a4918e3212dec1b7f1a07db683ecbca77a280d
Cosmin <cosmin_7d@yahoo.com>
2023-09-27 09:34:44 +0100

    fixed lock icon to be unlocked on default

--- public/js/lockToggle.js
+++ public/js/lockToggle.js
@@ -2,7 +2,7 @@
  const toggleLocks = document.querySelectorAll('#lockToggle')
      toggleLocks.forEach(toggleLock => {
          toggleLock.addEventListener('click',(e) => {
-             e.target.classList.toggle("bi-lock");
              e.target.classList.toggle("bi-unlock");
+             e.target.classList.toggle("bi-lock");
          })
      });
