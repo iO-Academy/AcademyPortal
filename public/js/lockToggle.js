4d94e45c024ccf6434aaf6bc5578c51fa767fa94
Cosmin <cosmin_7d@yahoo.com>
2023-09-29 10:37:58 +0100

    deleted the bootstrap link to the icons from composer.json and added catch in lockToggle.js

--- public/js/lockToggle.js
+++ public/js/lockToggle.js
@@ -1,10 +1,9 @@
 const urlParams = new URLSearchParams(window.location.search);
-const urlID = urlParams.get('id')
+const urlID = urlParams.get('id');
  document.querySelectorAll('#lockToggle').forEach(toggleLock => {
         toggleLock.addEventListener('click',(e) => {
-            const fieldName = toggleLock.dataset.field
-            const URL = 'http://localhost:8080/api/lockApplicantField?id=${urlID}&field=${fieldName}'
-            try {
+            const fieldName = toggleLock.dataset.field;
+            const URL = 'http://localhost:8080/api/lockApplicantField?id=${urlID}&field=${fieldName}';
                 fetch(URL)
                     .then(response => response.json())
                     .then(data => {
@@ -12,13 +11,13 @@ const urlID = urlParams.get('id')
                                 e.target.classList.toggle("bi-unlock");
                                 e.target.classList.toggle("bi-lock");
                         } else {
-                                document.querySelector(`#${fieldName}Error`).textContent = data.message
-                                document.querySelector(`#${fieldName}Error`).classList.toggle('hidden')
+                                document.querySelector(`#${fieldName}Error`).textContent = data.message;
+                                document.querySelector(`#${fieldName}Error`).classList.toggle('hidden');
                         }
-                    });
-            } catch (err) {
-                document.querySelector(`#${fieldName}Error`).innerHTML = "Error, the lock toggle not working"
-                document.querySelector(`#${fieldName}Error`).classList.toggle('hidden')
-            }
+                    })
+                    .catch(error => {
+                        document.querySelector(`#${fieldName}Error`).innerHTML = "Error, the lock toggle not working";
+                        document.querySelector(`#${fieldName}Error`).classList.toggle('hidden');
+                    })
         })
     });
