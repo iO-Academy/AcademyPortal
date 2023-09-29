a2bc60ec0893acc74e7b8e3fe9c232bd145c45d7
Cosmin <cosmin_7d@yahoo.com>
2023-09-29 10:31:49 +0100

    remove comments and fix code formatting

--- public/js/lockToggle.js
+++ public/js/lockToggle.js
@@ -1,25 +1,24 @@
-
-
-
 const urlParams = new URLSearchParams(window.location.search);
 const urlID = urlParams.get('id')
  document.querySelectorAll('#lockToggle').forEach(toggleLock => {
         toggleLock.addEventListener('click',(e) => {
             const fieldName = toggleLock.dataset.field
-            const URL = `http://localhost:8080/api/lockApplicantField?id=${urlID}&field=${fieldName}`
-            fetch(URL)
-                .then(response => response.json())
-                .then(data => {
-                    if (data.success) {
-                            // lock the icon
-                            e.target.classList.toggle("bi-unlock");
-
-                            e.target.classList.toggle("bi-lock");
-                    } else {
-                            document.querySelector(`#${fieldName}Error`).textContent = data.message
-                            document.querySelector(`#${fieldName}Error`).classList.toggle('hidden')
-                            // show error
-                    }
-                });
+            const URL = 'http://localhost:8080/api/lockApplicantField?id=${urlID}&field=${fieldName}'
+            try {
+                fetch(URL)
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.success) {
+                                e.target.classList.toggle("bi-unlock");
+                                e.target.classList.toggle("bi-lock");
+                        } else {
+                                document.querySelector(`#${fieldName}Error`).textContent = data.message
+                                document.querySelector(`#${fieldName}Error`).classList.toggle('hidden')
+                        }
+                    });
+            } catch (err) {
+                document.querySelector(`#${fieldName}Error`).innerHTML = "Error, the lock toggle not working"
+                document.querySelector(`#${fieldName}Error`).classList.toggle('hidden')
+            }
         })
     });
