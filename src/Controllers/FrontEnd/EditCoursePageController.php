1bb6c965967cd05c88e3aceafee2969f8e3b7fa6
Claudia Lim <climweiching@gmail.com>
2023-11-22 16:39:56 +0000

    create initial edit course page controller

---
+++ src/Controllers/FrontEnd/EditCoursePageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Portal\Controllers\FrontEnd;
+
+use Portal\Abstracts\Controller;
+use Portal\Models\CourseModel;
+use Slim\Views\PhpRenderer;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class EditCoursePageController extends Controller
+{
+    private CourseModel $courseModel;
+    private PhpRenderer $renderer;
+
+    public function __construct(CourseModel $courseModel, PhpRenderer $renderer)
+    {
+        $this->courseModel = $courseModel;
+        $this->renderer = $renderer;
+    }
+
+    public function __invoke(Request $request, Response $response, array $args): Response
+    {
+        $id = $request->getQueryParams()['id'];
+        $data['course'] = $this->courseModel->getCourseById($id);
+    }
+
+}
