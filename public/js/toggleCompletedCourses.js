b8ab75a53ffb07491cac373ad48a48c712adea2a
richardjgabb <richardjgabb@.com>
2024-04-23 09:48:19 +0100

    changed relative path

--- public/js/toggleCompletedCourses.js
+++ public/js/toggleCompletedCourses.js
@@ -2,7 +2,6 @@ document.querySelector('.completed-courses-btn').addEventListener('click', () =>
     document.querySelector('#completed-courses').classList.toggle('hidden')
 })
 
-
 document.addEventListener('DOMContentLoaded', function() {
     const dropdown = document.querySelector('.courseCategoryFilter');
     // Set the dropdown based on the URL parameter
@@ -14,6 +13,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // Event listener for changing the dropdown
     dropdown.addEventListener('change', () => {
         const selectedValue = dropdown.value;
-        window.location.href = `http://0.0.0.0:8080/courses?category=${selectedValue}`;
+        window.location.href = `/courses?category=${selectedValue}`;
     });
 });
