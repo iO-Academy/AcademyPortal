64797197de8a8292068587c2a4ceb556297b8ce4
Elizabeth Hartley <elizabeth.rae.gadsbygmail.com>
2024-04-24 14:31:25 +0100

    added student names on the course and drop down for reassigning student

--- public/js/deleteCourseModal.js
+++ public/js/deleteCourseModal.js
@@ -1,13 +1,36 @@
-function testingClick(){
-    console.log('testing click');
-}
-let deleteButtons  = document.querySelectorAll('.btnDeleteCourse');
+
+let deleteButtons = document.querySelectorAll('.btnDeleteCourse');
 for (let db of deleteButtons) {
-    let courseId = db.getAttribute('data-courseId');
-    console.log(courseId);
     db.addEventListener("click", () => {
-        const courseIdTag = document.querySelector('#courseId');
-        courseIdTag.innerHTML=courseId;
-        console.log(courseId)
+        const courseId= db.getAttribute('data-courseId');
+        displayDetails('./api/deleteCourse/'+ courseId);
     });
 }
+
+function displayDetails(url) {
+    fetch(url).then(
+        response => {
+            return response.json();
+        }).then(data => {
+            for (let student of data['data']) {
+                generateHTML(student['name']);
+            }
+        }
+    )
+}
+
+function generateHTML(name){
+    let studentNamesContainer = document.querySelector('#studentNamesContainer')
+    let studentName = document.createElement('h5');
+    studentName.innerText = name;
+    studentNamesContainer.appendChild(studentName);
+    let reassignContainer = document.querySelector('#reassignContainer');
+    let optionSelect = document.createElement('select');
+    let option = document.createElement('option');
+    option.innerText = 'select a course';
+    optionSelect.classList.add('select-margin');
+    optionSelect.appendChild(option);
+    reassignContainer.appendChild(optionSelect);
+}
+
+
