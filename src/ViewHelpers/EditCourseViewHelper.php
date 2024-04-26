70bb4af9ce5def5009b09bbc35cc9108a374bc81
Max Hamilton <maxeh987@gmail.com>
2024-04-26 13:28:52 +0100

    fixed course edit page showing wrong category

--- src/ViewHelpers/EditCourseViewHelper.php
+++ src/ViewHelpers/EditCourseViewHelper.php
@@ -4,13 +4,13 @@ namespace Portal\ViewHelpers;
 
 class EditCourseViewHelper
 {
-    public static function courseEditCategoryDropdown(array $categories, int $courseCatId): string
+    public static function courseEditCategoryDropdown(array $categories, string $courseCatId): string
     {
         $courseCategoryDropdown = '';
         foreach ($categories as $category) {
-            $selected = $category['id'] === $courseCatId ? 'selected' : '';
+            $selected = $category['id'] == $courseCatId ? 'selected' : '';
             $courseCategoryDropdown .=
-                '<option selected="' . $selected . '" value="' . $category['id'] . '">'
+                '<option ' . $selected . ' value="' . $category['id'] . '">'
                 . $category['category'] . '</option>';
         }
         return $courseCategoryDropdown;
