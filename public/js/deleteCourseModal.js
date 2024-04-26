3b1c1798275b97d3df0dba5dbf9bc1016429d047
Elizabeth Hartley <elizabeth.rae.gadsbygmail.com>
2024-04-26 12:15:10 +0100

    all trainer comments resolved, still fully functioning

--- public/js/deleteCourseModal.js
+++ public/js/deleteCourseModal.js
@@ -1,43 +1,7 @@
-let deleteButtons = document.querySelectorAll('.btnDeleteCourse');
-for (let db of deleteButtons) {
-    db.addEventListener("click", () => {
-        const courseId = db.getAttribute('data-courseId');
-        displayDetails('./api/deleteCourse/' + courseId, courseId);
-    });
-}
-
-function displayDetails(url, courseId) {
-    fetch(url).then(
-        response => {
-            return response.json();
-        }).then(data => {
-            let studentNamesContainer = document.querySelector('#studentNamesContainer');
-            studentNamesContainer.innerHTML = '<h4>Name</h4>';
-            let DeleteCoursesForm = document.querySelector('#DeleteCoursesForm');
-            DeleteCoursesForm.innerHTML = '';
-            DeleteCoursesForm.action = "api/deleteCourse/" + courseId;
-            let prettyCourses = makeCoursesPretty(data['availableCourses'])
-            for (let student of data['applicants']) {
-                generateStudents(student['name'], studentNamesContainer);
-                generateOptions(prettyCourses, DeleteCoursesForm, student['id'], data['availableCourses']);
-            }
-        }
-    )
-}
-
 function generateStudents(name, studentNamesContainer) {
     let studentName = document.createElement('h5');
     studentName.innerText = name;
     studentNamesContainer.appendChild(studentName);
-}
-
-function makeCoursesPretty(courses) {
-    let prettyCoursesArray = [];
-    for (let course of courses) {
-        let prettyCourse = course['name'] + ' ' + course['start_date'];
-        prettyCoursesArray.push(prettyCourse);
-    }
-    return prettyCoursesArray;
 }
 
 function generateOptions(prettyCourses, DeleteCoursesForm, studentId, courses) {
@@ -56,4 +20,40 @@ function generateOptions(prettyCourses, DeleteCoursesForm, studentId, courses) {
         optionSelect.appendChild(option);
     }
     DeleteCoursesForm.appendChild(optionSelect)
+}
+
+function displayDetails(url) {
+    fetch(url).then(
+        response => {
+            return response.json();
+        }).then(data => {
+            let studentNamesContainer = document.querySelector('#studentNamesContainer');
+            studentNamesContainer.innerHTML = '<h4>Name</h4>';
+            let DeleteCoursesForm = document.querySelector('#DeleteCoursesForm');
+            DeleteCoursesForm.innerHTML = '';
+            DeleteCoursesForm.action = url;
+            let prettyCourses = formatCourseStartAndName(data['availableCourses'])
+            for (let student of data['applicants']) {
+                generateStudents(student['name'], studentNamesContainer);
+                generateOptions(prettyCourses, DeleteCoursesForm, student['id'], data['availableCourses']);
+            }
+        }
+    )
+}
+
+let deleteButtons = document.querySelectorAll('.btnDeleteCourse');
+for (let db of deleteButtons) {
+    db.addEventListener("click", () => {
+        const courseId = db.getAttribute('data-courseId');
+        displayDetails('./api/deleteCourse/' + courseId, courseId);
+    });
+}
+
+function formatCourseStartAndName(courses) {
+    let courseStartAndNameArray = [];
+    for (let course of courses) {
+        let courseStartAndName = course['name'] + ' ' + course['start_date'];
+        courseStartAndNameArray.push(courseStartAndName);
+    }
+    return courseStartAndNameArray;
 }
