1df11b4ac6fb6528a71ee2974a8383f98813c76d
Carl Ollington <Carl.Ollington@outlook.com>
2024-09-24 13:50:05 +0100

    Dry Cookies

--- public/js/stagesPage.js
+++ public/js/stagesPage.js
@@ -1,8 +1,6 @@
 const editButtons = document.querySelectorAll('.toggleEditForm');
 const editForms = document.querySelectorAll('.stagesTableForm');
-const editResponse = document.getElementById('editResponse'); // @todo: can we remove this?
 const newStageForm = document.getElementById('newStageForm');
-const createNewResponse = document.getElementById('createNewResponse'); // @todo: can we remove this?
 const deleteButtons = document.querySelectorAll('.delete');
 const optionButtons = document.querySelectorAll('.toggleEditOptions');
 const optionEditSubmits = document.querySelectorAll('.optionEditSubmit');
@@ -107,33 +105,6 @@ optionAddSubmits.forEach((optionAddSubmit) => {
         window.location.reload()
     })
 })
-
-window.addEventListener('load', (event) => {
-    checkCookie()
-});
-
-function checkCookie() {
-    let elem = document.getElementById('editResponse');
-    let response = getCookie("response");
-    elem.textContent = response;
-    document.cookie = "response=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
 
 optionButtons.forEach((optionButton) => {
     optionButton.addEventListener('click', (e) => {
