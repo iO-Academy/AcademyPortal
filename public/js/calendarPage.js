54567efa01969cb0453cb406232920479bc01b1e
Henry Tottle <henrywilliamtottle@gmail.com>
2024-09-24 11:27:35 +0100

    calendar click started

--- public/js/calendarPage.js
+++ public/js/calendarPage.js
@@ -1,5 +1,21 @@
 const calendarEl = document.getElementById("calendar");
 const calendar = new FullCalendar.Calendar(calendarEl, {
+  eventClick: function(info) {
+    var eventObj = info.event;
+    console.log(eventObj)
+    if (eventObj.url) {
+      alert(
+          'Clicked ' + eventObj.title + '.\n' +
+          'Will open ' + eventObj.url + ' in a new tab'
+      );
+
+      window.open(eventObj.url);
+
+      info.jsEvent.preventDefault(); // prevents browser from following link in current tab.
+    } else {
+      alert('Clicked ' + eventObj.title);
+    }
+  },
   initialView: "dayGridMonth",
   businessHours: {'daysOfWeek': [1,2,3,4,5], 'startTime': '09:00', 'endTime': '17:00' },
   eventTimeFormat: {
