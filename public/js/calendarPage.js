0cef0b6ec3fb73d476f2638e0b2b39ecfca3bc08
Henry Tottle <henrywilliamtottle@gmail.com>
2024-09-25 10:44:09 +0100

    learned us some JQuery, got the modal working like other modals existing

--- public/js/calendarPage.js
+++ public/js/calendarPage.js
@@ -2,35 +2,17 @@ const calendarEl = document.getElementById("calendar");
 const calendar = new FullCalendar.Calendar(calendarEl, {
   eventClick: function(info) {
     var eventObj = info.event;
-    eventObj.extendedProps = undefined;
-    console.log(eventObj)
-    const calendarEventModal= document.getElementById("myModal")
-    calendarEventModal.showModal()
-    const closeModal = document.getElementById('close')
-
-    closeModal.addEventListener("click", () => {
-      calendarEventModal.close()
-    })
-
-    const eventName = document.getElementById('eventName')
-    const eventLocation = document.getElementById('eventLocation')
-    const eventDate = document.getElementById('eventDate')
-    const eventStart = document.getElementById('eventStart')
-    const eventEnd = document.getElementById('eventEnd')
-
-
-
-
-
-
-    eventName.innerText = 'Event: ' + eventObj.title
-    eventLocation.innerText = 'Location: ' + eventObj.extendedProps.location
-    eventDate.innerText = 'Date: ' + eventObj.date
-    eventStart.innerText = 'Start: ' + eventObj.start
-    eventEnd.innerText = 'End: ' + eventObj.end
-
-
-
+    $('#modalTitle').html('Event Details');
+    $('#modalBody').html(event.description);
+    $('#eventName').html('Event: ' + eventObj.title);
+    $('#eventLocation').html('Location: ' + eventObj.extendedProps.location)
+    $('#eventStart').html('Start: ' + eventObj.start)
+    if (eventObj.end) {
+      $('#eventEnd').html('End: ' + eventObj.end)
+    } else {
+      $('#eventEnd').html('')
+    }
+    $('#calendarModal').modal();
   },
   initialView: "dayGridMonth",
   businessHours: {'daysOfWeek': [1,2,3,4,5], 'startTime': '09:00', 'endTime': '17:00' },
