ee556f311926b16516bbd3a03b2027a3dafd7c2c
Carl Ollington <Carl.Ollington@outlook.com>
2024-09-25 14:02:20 +0100

    Course Header Sort Functionality

--- public/js/toggleCompletedCourses.js
+++ public/js/toggleCompletedCourses.js
@@ -2,17 +2,31 @@ document.querySelector('.completed-courses-btn').addEventListener('click', () =>
     document.querySelector('#completed-courses').classList.toggle('hidden')
 })
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const dropdown = document.querySelector('.courseCategoryFilter');
+    const sortColumn = document.querySelectorAll('.sortHeader');
+
     // Set the dropdown based on the URL parameter
-    const urlParams = new URLSearchParams(window.location.search);
+    const
+        urlParams = new URLSearchParams(window.location.search);
     const categoryParam = urlParams.get('category');
     if (categoryParam) {
         dropdown.value = categoryParam;
     }
+
     // Event listener for changing the dropdown
     dropdown.addEventListener('change', () => {
-        const selectedValue = dropdown.value;
-        window.location.href = `/courses?category=${selectedValue}`;
+        const dropdownValue = dropdown.value;
+        const headerValue = urlParams.get('sortColumn');
+        window.location.href = `/courses?category=${dropdownValue}&sortColumn=${headerValue}`;
     });
+
+    // Event listener for ordering the data by header value
+    sortColumn.forEach((item) => {
+        item.addEventListener('click', () => {
+            const headerValue = item.value;
+            const dropdownValue = dropdown.value;
+            window.location.href = `/courses?category=${dropdownValue}&sortColumn=${headerValue}`
+        })
+    })
 });
