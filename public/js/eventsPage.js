e55ff65c1250a6797fb3d31e70b8b8319da8bbb2
Emma Ferrari <e.ferrari@live.co.uk>
2024-09-25 15:07:02 +0100

    Added button, created phtml page

--- public/js/eventsPage.js
+++ public/js/eventsPage.js
@@ -250,6 +250,7 @@ async function eventGenerator(event, hiringPartners, applicants) {
             `</h4> 
             <button class="delete-btn btn">Delete</button>
             <button class="show-event-info btn" data-button-id='button${event.id}' data-reference='${event.id}'>More Info</button>
+            <button class="edit-button btn" data-button-id='button${event.id}' data-reference='${event.id}'>Edit</button>
         </div>
         <div id="moreInfo${event.id}" class="hidden moreInfo">
         <p><span>Event Category:</span> ${event.category_name}</p>
