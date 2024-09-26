0e7272bc2fc6853ee2f58c3d6b7fc7e0f17fdc17
Henry Tottle <henrywilliamtottle@gmail.com>
2024-09-26 11:33:46 +0100

    re added code to prevent previous working if page number is less than 1

--- public/js/eventsPage.js
+++ public/js/eventsPage.js
@@ -21,7 +21,9 @@ const pageClickFunction = () => {
 }
 
 document.querySelector('#counterMinus').addEventListener('click', function (e) {
-    pageNumber--
+    if (pageNumber > 1) {
+        pageNumber--
+    }
     pageClickFunction()
 })
 
